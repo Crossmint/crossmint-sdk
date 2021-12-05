361d27446fb015cccb2e5649470e4395a57dd59f
Maxwell Fortney <maxwellfortney@gmail.com>
2021-12-04 19:08:19 -0500

    Adds ability to pass collection information to checkout flow

--- packages/starter/nextjs-starter/pages/index.tsx
+++ packages/starter/nextjs-starter/pages/index.tsx
@@ -5,7 +5,12 @@ export default function Index(): JSX.IntrinsicAttributes {
         <div
             style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}
         >
-            <CrossMintButton candyMachineId="<CANDY_MACHINE_ID>" />
+            <CrossMintButton
+                candyMachineId="<CANDY_MACHINE_ID>"
+                collectionTitle="<TITLE_FOR_YOUR_COLLECTION>"
+                collectionDescription="<DESCRIPTION_OF_YOUR_COLLECTION>"
+                collectionPhoto="<URL_TO_A_PHOTO>"
+            />
         </div>
     );
 }
