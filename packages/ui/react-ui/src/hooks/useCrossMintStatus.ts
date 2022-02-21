5dfc1d31d6df7a958de770d0722941ba870ec64a
Pablo Egido <pgarciaegido@gmail.com>
2022-02-21 18:26:10 +0100

    change validation and modify console error

--- packages/ui/react-ui/src/hooks/useCrossMintStatus.ts
+++ packages/ui/react-ui/src/hooks/useCrossMintStatus.ts
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { validate } from "uuid";
+import { PublicKey } from "@solana/web3.js";
 
 export enum OnboardingRequestStatusResponse {
     WAITING_SUBMISSION = "waiting-submission",
@@ -19,8 +21,15 @@ interface IProps {
     clientId: string;
 }
 
-const isValidUUID = (uuid: string) =>
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(uuid);
+const validateClientId = (clientId: string): boolean => {
+    try {
+        const isValid = validate(clientId) || new PublicKey(clientId);
+        return !!isValid;
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+};
 
 export default function useCrossMintStatus({ clientId }: IProps) {
     const [status, setStatus] = useState<OnboardingRequestStatusResponse>(
@@ -33,9 +42,9 @@ export default function useCrossMintStatus({ clientId }: IProps) {
             return;
         }
 
-        if (!isValidUUID(clientId)) {
+        if (!validateClientId(clientId)) {
             console.error(
-                "Entered `clientId` is not valid. It must be a valid Crossmint client ID. It will have the following format: `00000000-0000-0000-0000-00000000`."
+                "The clientId passed to is invalid. Make sure to pass the clientId obtained from the crossmint team, with format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX"
             );
             return;
         }
