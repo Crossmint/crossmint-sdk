e745439e7d9acdb0481f7ad344e6388c0e6cfcbb
Pablo Egido <pgarciaegido@gmail.com>
2022-02-28 21:03:06 +0100

    Adding github action on PR

--- packages/starter/nextjs-starter/pages/index.tsx
+++ packages/starter/nextjs-starter/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
                 collectionTitle="<TITLE_FOR_YOUR_COLLECTION>"
                 collectionDescription="<DESCRIPTION_OF_YOUR_COLLECTION>"
                 collectionPhoto="<OPT_URL_TO_PHOTO_COVER>"
-                clientId="<YOUR_CLIENT_ID>"
+                clientId="7e94eb71-06c0-447f-9272-677416fee1d5"
             />
         </div>
     );
