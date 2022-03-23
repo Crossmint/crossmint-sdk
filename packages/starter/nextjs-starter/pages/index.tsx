3f18a82584b7129db4d84048df9d6a3c32eaf2c5
Maxwell Fortney <maxwell@paella.dev>
2022-03-23 16:37:24 -0400

    Adds environment parameter to buttons

--- packages/starter/nextjs-starter/pages/index.tsx
+++ packages/starter/nextjs-starter/pages/index.tsx
@@ -1,3 +1,4 @@
+import { baseUrls } from "@crossmint/client-sdk-base";
 import { CrossmintPayButton } from "@crossmint/client-sdk-react-ui";
 
 export default function Index() {
@@ -16,6 +17,7 @@ export default function Index() {
                 collectionDescription="<DESCRIPTION_OF_YOUR_COLLECTION>"
                 collectionPhoto="<OPT_URL_TO_PHOTO_COVER>"
                 clientId="7e94eb71-06c0-447f-9272-677416fee1d5"
+                environment={baseUrls.staging}
             />
         </div>
     );
