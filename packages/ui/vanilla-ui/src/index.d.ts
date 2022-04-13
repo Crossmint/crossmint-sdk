bebc3a959dce7c5059fe5cc747ea311241bf03db
Pablo Egido <pgarciaegido@gmail.com>
2022-04-12 19:25:01 -0500

    Typescript declaration (#93)

---
+++ packages/ui/vanilla-ui/src/index.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            "crossmint-pay-button": any;
+        }
+    }
+}
+
+// Little hack for the types to work.
+// Looks like if you don't export anything, its not recognized.
+declare const _: any;
+
+export { _ };
