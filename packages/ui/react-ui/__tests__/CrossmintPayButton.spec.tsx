d39cec806b77c0e8ab5d962f63a1e0fa639b5800
Pablo Egido <peg@paella.dev>
2023-02-20 17:03:07 +0100

    Fix test (#145)

--- packages/ui/react-ui/__tests__/CrossmintPayButton.spec.tsx
+++ packages/ui/react-ui/__tests__/CrossmintPayButton.spec.tsx
@@ -41,7 +41,7 @@ describe("CrossmintPayButton", () => {
         expect(global.open).toHaveBeenCalledWith(
             `https://www.crossmint.io/signin?callbackUrl=https%3A%2F%2Fwww.crossmint.io%2Fcheckout%2Fmint%3FclientId%3D${encodeURIComponent(
                 defaultProps.clientId
-            )}%26clientName%3Dclient-sdk-react-ui%26clientVersion%3D${LIB_VERSION}%26mintConfig%3D%257B%2522type%2522%253A%2522candy-machine%2522%257D`,
+            )}%26clientName%3Dclient-sdk-react-ui%26clientVersion%3D${LIB_VERSION}%26mintConfig%3D%257B%2522type%2522%253A%2522candy-machine%2522%257D%26locale%3Den-US%26currency%3DUSD`,
             "popUpWindow",
             "height=750,width=400,left=312,top=9,resizable=yes,scrollbars=yes,toolbar=yes,menubar=true,location=no,directories=no, status=yes"
         );
