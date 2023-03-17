03620cd8337370d80e89fb57c2e720fade5e5b17
Pablo Egido <peg@paella.dev>
2023-03-17 18:22:04 +0100

    Add vue starter

---
+++ packages/starter/vue-ui-starter/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { CrossmintPaymentElement } from "@crossmint/client-sdk-vue-ui";
+</script>
+
+<template>
+    <p>Hola</p>
+    <CrossmintPaymentElement></CrossmintPaymentElement>
+</template>
+
+<style scoped></style>
