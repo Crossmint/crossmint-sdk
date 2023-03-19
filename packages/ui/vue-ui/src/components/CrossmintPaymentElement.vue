b327344e96e45a435e7f6ca6a5edc05af43f8b81
Pablo Egido <peg@paella.dev>
2023-03-19 17:47:40 +0100

    Adding dts vue UI (#170)
    * Adding d.ts files in vue ui bundle
    
    * changes
    
    * change script name
    
    * Improve naming
    
    * exclude vue packages
    
    * mint args optional
    
    * Create CheckoutEventMap
    
    * Update vue component type

--- packages/ui/vue-ui/src/components/CrossmintPaymentElement.vue
+++ packages/ui/vue-ui/src/components/CrossmintPaymentElement.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-console.log("hola");
-interface hola {
-    adios: string;
+import type { CheckoutEventMap, Currency, Locale, PaymentMethod } from "@crossmint/client-sdk-base";
+
+// TODO: Looks like you cannot import the interface directly from the package
+// https://github.com/vuejs/core/issues/4294#issuecomment-970861525
+export interface PaymentElement {
+    clientId: string;
+    mintArgs?: Record<string, any>;
+    recipient?: string;
+    paymentMethod?: PaymentMethod;
+    currency?: Currency;
+    locale?: Locale;
+    onEvent?<K extends keyof CheckoutEventMap>(event: K, payload: CheckoutEventMap[K]): this;
 }
 
-const saludo: hola = {
-    adios: "adios!!!!",
-};
-console.log(saludo);
+const props = withDefaults(defineProps<PaymentElement>(), {});
+console.log(props);
 </script>
 
 <template>
-    <p>Hola!!!!!!</p>
+    <p>Hola!!!!!! {{ JSON.stringify(props) }}</p>
 </template>
 
 <style scoped>
--- packages/ui/vue-ui/vite.config.ts
+++ packages/ui/vue-ui/vite.config.ts
@@ -1,9 +1,15 @@
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        dts({
+            tsConfigFilePath: "tsconfig.app.json",
+        }),
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, "./src/main.ts"),
