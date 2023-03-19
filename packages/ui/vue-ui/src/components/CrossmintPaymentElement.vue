2fafef6fc1fa094b184b4bd52d0a94afd0347de7
Pablo Egido <peg@paella.dev>
2023-03-19 17:43:54 +0100

    Update vue component type

--- packages/ui/vue-ui/src/components/CrossmintPaymentElement.vue
+++ packages/ui/vue-ui/src/components/CrossmintPaymentElement.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import type { Currency, Locale, PaymentMethod } from "@crossmint/client-sdk-base";
+import type { CheckoutEventMap, Currency, Locale, PaymentMethod } from "@crossmint/client-sdk-base";
 
 // TODO: Looks like you cannot import the interface directly from the package
 // https://github.com/vuejs/core/issues/4294#issuecomment-970861525
 export interface PaymentElement {
     clientId: string;
-    mintArgs: Record<string, any>;
+    mintArgs?: Record<string, any>;
     recipient?: string;
-    pamentMethod?: PaymentMethod;
+    paymentMethod?: PaymentMethod;
     currency?: Currency;
     locale?: Locale;
-    onEvent?: (event: string, metadata: Record<string, any>) => void;
+    onEvent?<K extends keyof CheckoutEventMap>(event: K, payload: CheckoutEventMap[K]): this;
 }
 
 const props = withDefaults(defineProps<PaymentElement>(), {});
