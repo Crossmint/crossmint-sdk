097c6eaa34f1adfdbdbdc90a0b1daf6ce132e29e
Pablo Egido <peg@paella.dev>
2023-03-23 11:54:09 +0100

    Modify listen to minting events

--- packages/core/base/src/services/useCrossmintEvents.ts
+++ packages/core/base/src/services/useCrossmintEvents.ts
@@ -6,30 +6,14 @@ export default function useCrossmintEvents() {
         const emittedEvents: CheckoutEvents[] = [];
 
         const timer = setInterval(async () => {
-            const status = await fetchOrderStatus({ orderIdentifier });
-            switch (status) {
-                case CheckoutEvents.MINTING_STARTED:
-                    if (!emittedEvents.includes(CheckoutEvents.MINTING_STARTED)) {
-                        cb({ type: CheckoutEvents.MINTING_STARTED, payload: {} });
-                        emittedEvents.push(CheckoutEvents.MINTING_STARTED);
-                    }
-                    break;
-                case CheckoutEvents.MINTING_COMPLETED:
-                    if (!emittedEvents.includes(CheckoutEvents.MINTING_COMPLETED)) {
-                        cb({ type: CheckoutEvents.MINTING_COMPLETED, payload: {} });
-                        emittedEvents.push(CheckoutEvents.MINTING_COMPLETED);
-                    }
-                    clearInterval(timer);
-                    break;
-                case CheckoutEvents.MINTING_FAILED:
-                    if (!emittedEvents.includes(CheckoutEvents.MINTING_FAILED)) {
-                        cb({ type: CheckoutEvents.MINTING_FAILED, payload: {} });
-                        emittedEvents.push(CheckoutEvents.MINTING_FAILED);
-                    }
-                    clearInterval(timer);
-                    break;
-                default:
-                    break;
+            const event = await fetchOrderStatus({ orderIdentifier });
+            if (emittedEvents.includes(event.type)) {
+                return;
+            }
+            cb(event);
+            emittedEvents.push(event.type);
+            if (event.type === CheckoutEvents.MINTING_COMPLETED) {
+                clearInterval(timer);
             }
         }, 5000);
     }
