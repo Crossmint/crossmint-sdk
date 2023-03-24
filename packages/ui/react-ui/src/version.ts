1c4e146ef5bd5aa377c01005305a14decbb7ee5e
Maxwell Fortney <maxwell@paella.dev>
2023-03-23 23:29:22 -0400

    Publish
     - @crossmint/client-sdk-base@0.2.7-alpha.6
     - @crossmint/client-sdk-example@0.1.11-alpha.6
     - @crossmint/client-sdk-nextjs-starter@0.1.11-alpha.6
     - @crossmint/client-sdk-react-ui-starter@0.1.11-alpha.6
     - @crossmint/client-sdk-vue-ui-starter@0.0.1-alpha.6
     - @crossmint/client-sdk-react-ui@0.2.7-alpha.6
     - @crossmint/client-sdk-vanilla-ui@0.1.7-alpha.6
     - @crossmint/client-sdk-vue-ui@0.0.1-alpha.6

--- packages/ui/react-ui/src/version.ts
+++ packages/ui/react-ui/src/version.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.2.7-alpha.5";
+export const LIB_VERSION = "0.2.7-alpha.6";
--- packages/ui/vanilla-ui/src/version.ts
+++ packages/ui/vanilla-ui/src/version.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.1.7-alpha.5";
+export const LIB_VERSION = "0.1.7-alpha.6";
