8a543e7a040be3fa1f5adb873545f6a120d90f35
Pablo Egido <peg@paella.dev>
2023-03-24 14:02:29 +0100

    Add wallet in starter

--- packages/starter/vue-ui-starter/src/components/PayWithCC.vue
+++ packages/starter/vue-ui-starter/src/components/PayWithCC.vue
@@ -98,7 +98,7 @@ function onEvent(event: CrossmintCheckoutEvent) {
             <CrossmintPaymentElement
                 environment="http://localhost:3000"
                 clientId="db218e78-d042-4761-83af-3c4e5e6659dd"
-                :recipient="{ email }"
+                :recipient="{ email, wallet }"
                 :on-event="onEvent"
             />
         </div>
