859baa2540eb66e4214d646a7775ff9115596431
Pablo Egido <peg@paella.dev>
2023-03-26 20:58:56 +0200

    Add Payment Element in React (#193)

--- packages/ui/react-ui/src/index.ts
+++ packages/ui/react-ui/src/index.ts
@@ -2,3 +2,6 @@ export * from "./CrossmintPayButton";
 export * from "./CrossmintStatusButton";
 export * from "./CrossmintNFTCollectionView";
 export * from "./CrossmintNFTDetail";
+export * from "./CrossmintPaymentElement";
+export { CheckoutEvents, useCrossmintEvents } from "@crossmint/client-sdk-base";
+export type { CrossmintCheckoutEvent, CheckoutEventMap } from "@crossmint/client-sdk-base";
