5ec8fd5ead4c0314f1c5b5121e17c41d980c7a6a
Pablo Egido <peg@paella.dev>
2023-03-26 18:19:47 +0200

    Add vanilla Crossmint Payment Element component

--- packages/ui/vanilla-ui/src/index.ts
+++ packages/ui/vanilla-ui/src/index.ts
@@ -1,2 +1,5 @@
 export * from "./CrossmintPayButton";
 export * from "./CrossmintStatusButton";
+export * from "./CrossmintPaymentElement";
+export { CheckoutEvents, useCrossmintEvents } from "@crossmint/client-sdk-base";
+export type { CrossmintCheckoutEvent, CheckoutEventMap } from "@crossmint/client-sdk-base";
