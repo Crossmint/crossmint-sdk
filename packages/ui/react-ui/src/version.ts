b1e507329470f40e0ed735d88a4a5d2c249cfb63
Pablo Egido <peg@paella.dev>
2023-04-20 10:24:14 +0200

    Publish
     - @crossmint/client-sdk-base@0.2.7-alpha.26
     - @crossmint/client-sdk-example@0.1.11-alpha.26
     - @crossmint/client-sdk-nextjs-starter@0.1.11-alpha.26
     - @crossmint/client-sdk-react-ui-starter@0.1.11-alpha.26
     - @crossmint/client-sdk-vue-ui-starter@0.0.1-alpha.26
     - @crossmint/client-sdk-react-ui@0.2.7-alpha.26
     - @crossmint/client-sdk-vanilla-ui@0.1.7-alpha.26
     - @crossmint/client-sdk-vue-ui@0.0.1-alpha.26

--- packages/ui/react-ui/src/version.ts
+++ packages/ui/react-ui/src/version.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.2.7-alpha.25";
+export const LIB_VERSION = "0.2.7-alpha.26";
--- packages/ui/vanilla-ui/src/version.ts
+++ packages/ui/vanilla-ui/src/version.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.1.7-alpha.25";
+export const LIB_VERSION = "0.1.7-alpha.26";
