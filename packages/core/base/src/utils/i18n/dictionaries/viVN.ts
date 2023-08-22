66926e8fd6e70080a96341b9ba3e691564fcefb5
guillermodoghel <guillermo@paella.dev>
2023-08-21 21:21:11 -0300

    Add support for Vietnamese

---
+++ packages/core/base/src/utils/i18n/dictionaries/viVN.ts
@@ -0,0 +1,9 @@
+const enUS = {
+    crossmintPayButtonService: {
+        CONNECTING: "Đang kết nối",
+        BUY_WITH_ETH: "Mua bằng ETH",
+        BUY_WITH_SOL: "Mua bằng SOL",
+        BUY_WITH_CREDIT_CARD: "Mua bằng thẻ tín dụng",
+    },
+};
+export default enUS;
