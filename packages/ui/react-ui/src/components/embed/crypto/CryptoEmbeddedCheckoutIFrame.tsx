b693e8d6b4745977994d9e7e3a476e1365e8dcbe
Maxwell Fortney <maxwell@paella.dev>
2023-09-14 15:32:34 -0400

    save

--- packages/ui/react-ui/src/components/embed/crypto/CryptoEmbeddedCheckoutIFrame.tsx
+++ packages/ui/react-ui/src/components/embed/crypto/CryptoEmbeddedCheckoutIFrame.tsx
@@ -1,6 +1,10 @@
+import bs58 from "bs58";
+
 import {
     CryptoEmbeddedCheckoutPropsWithSigner,
+    ETHEmbeddedCheckoutSigner,
     IncomingInternalEvent,
+    SOLEmbeddedCheckoutSigner,
     crossmintIFrameService,
 } from "@crossmint/client-sdk-base";
 
@@ -9,20 +13,31 @@ import CrossmintEmbeddedCheckoutIFrame from "../EmbeddedCheckoutIFrame";
 export default function CryptoEmbeddedCheckoutIFrame(props: CryptoEmbeddedCheckoutPropsWithSigner) {
     const { emitInternalEvent } = crossmintIFrameService(props);
 
-    const { signer } = props;
+    const { signer, paymentMethod } = props;
 
     function onInternalEvent(event: IncomingInternalEvent) {
         const { type, payload } = event;
 
         if (type === "crypto-payment:incoming-transaction") {
             const { serializedTransaction } = payload;
+            console.log("[Crossmint] Received incoming transaction", serializedTransaction);
             handleIncomingTransaction(serializedTransaction);
         }
     }
 
     async function handleIncomingTransaction(serializedTransaction: string) {
         try {
-            const txId = await signer.signAndSendTransaction(serializedTransaction);
+            let txId: string;
+            switch (paymentMethod) {
+                case "SOL":
+                    txId = await handleSOLTransaction(signer, serializedTransaction);
+                    break;
+                case "ETH":
+                    txId = await handleETHTransaction(signer, serializedTransaction);
+                    break;
+                default:
+                    throw new Error(`Unsupported payment method ${paymentMethod}`);
+            }
 
             console.log("[Crossmint] Signed and sent transaction", txId);
             emitInternalEvent({
@@ -38,6 +53,24 @@ export default function CryptoEmbeddedCheckoutIFrame(props: CryptoEmbeddedChecko
                 payload: {},
             });
         }
+    }
+
+    async function handleSOLTransaction(signer: SOLEmbeddedCheckoutSigner, serializedTransaction: string) {
+        // @ts-expect-error - Error becasue we dont use 'module' field in tsconfig, which is expected because we use tsup to compile
+        const { Transaction } = await import("@solana/web3.js");
+        const transaction = Transaction.from(bs58.decode(serializedTransaction));
+        console.log("[Crossmint] Deserialized SOL transaction", transaction);
+
+        return await signer.signAndSendTransaction(transaction);
+    }
+
+    async function handleETHTransaction(signer: ETHEmbeddedCheckoutSigner, serializedTransaction: string) {
+        // @ts-expect-error - Error becasue we dont use 'module' field in tsconfig, which is expected because we use tsup to compile
+        const { parse: parseTransaction } = await import("@ethersproject/transactions");
+        const transaction = parseTransaction(serializedTransaction);
+        console.log("[Crossmint] Deserialized ETH transaction", transaction);
+
+        return await signer.signAndSendTransaction(transaction);
     }
 
     return <CrossmintEmbeddedCheckoutIFrame onInternalEvent={onInternalEvent} {...props} />;
