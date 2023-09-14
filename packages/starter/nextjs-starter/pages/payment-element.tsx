a01d17e07cdbe483b065cbb8144e4fb1132ada21
Maxwell Fortney <maxwell@paella.dev>
2023-09-13 23:50:46 -0400

    testing page

--- packages/starter/nextjs-starter/pages/payment-element.tsx
+++ packages/starter/nextjs-starter/pages/payment-element.tsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
+
 import { CrossmintPaymentElement } from "@crossmint/client-sdk-react-ui";
 
 export default function PaymentElementPage() {
+    const [count, setCount] = useState(1);
+
     return (
-        <CrossmintPaymentElement
-            environment="https://crossmint-main-git-checkout-embedded-new-element-p2-crossmint.vercel.app"
-            clientId="db218e78-d042-4761-83af-3c4e5e6659dd"
-            mintConfig={{ totalPrice: "0.001", quantity: "1" }}
-            recipient={{ wallet: "0xdC9bb9929b79b62d630A7C3568c979a2843eFd8b" }}
-            paymentMethod="ETH"
-            signer={{
-                address: "0xdC9bb9929b79b62d630A7C3568c979a2843eFd8b",
-                signAndSendTransaction: async (tx) => {
-                    return "0x1234";
-                },
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                gap: "20px",
             }}
-            onEvent={(event) => {
-                console.log(event);
-            }}
-        />
+        >
+            <button onClick={() => setCount(count + 1)}>Increment count: {count}</button>
+            <CrossmintPaymentElement
+                environment="https://crossmint-main-git-checkout-embedded-new-element-p3-crossmint.vercel.app"
+                clientId="db218e78-d042-4761-83af-3c4e5e6659dd"
+                recipient={{ wallet: "maxfQWBno84Zfu4sXgmjYvsvLn4LzGFSgSkFMFuzved" }}
+                mintConfig={{
+                    testCount: count,
+                }}
+                paymentMethod="ETH"
+                signer={{
+                    address: "0xdC9bb9929b79b62d630A7C3568c979a2843eFd8b",
+                    signAndSendTransaction: async (tx) => {
+                        return "0x1234";
+                    },
+                }}
+                onEvent={(event) => {
+                    console.log(event);
+                }}
+            />
+        </div>
     );
 }
