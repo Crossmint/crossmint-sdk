76086c1f143f841522234819750000e7e24fddd5
Maxwell Fortney <maxwell@paella.dev>
2023-09-18 11:55:44 -0400

    connect and pay example

--- packages/starter/nextjs-starter/pages/payment-element.tsx
+++ packages/starter/nextjs-starter/pages/payment-element.tsx
@@ -1,38 +1,91 @@
-import { useState } from "react";
+import {
+    DynamicConnectButton,
+    DynamicContextProvider,
+    DynamicWidget,
+    useDynamicContext,
+} from "@dynamic-labs/sdk-react";
+import { useEffect, useState } from "react";
 
 import { CrossmintPaymentElement } from "@crossmint/client-sdk-react-ui";
 
 export default function PaymentElementPage() {
     const [count, setCount] = useState(1);
+    const [signer, setSigner] = useState<any>(null);
 
     return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                width: "100%",
-                gap: "20px",
+        <DynamicContextProvider
+            settings={{
+                initialAuthenticationMode: "connect-only",
+                environmentId: "377e1f17-8ef9-4a9a-b35c-6a13ffb1de5e",
+                eventsCallbacks: {
+                    onConnectSuccess: async ({ walletConnector }) => {
+                        const _signer = await walletConnector?.getSigner();
+                        console.log("_signer", _signer);
+                        setSigner(_signer);
+                    },
+                },
             }}
         >
-            <button onClick={() => setCount(count + 1)}>Increment count: {count}</button>
-            <CrossmintPaymentElement
-                environment="https://crossmint-main-git-checkout-embedded-new-element-p3-crossmint.vercel.app"
-                clientId="db218e78-d042-4761-83af-3c4e5e6659dd"
-                recipient={{ wallet: "maxfQWBno84Zfu4sXgmjYvsvLn4LzGFSgSkFMFuzved" }}
-                mintConfig={{
-                    testCount: count,
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                    gap: "20px",
                 }}
-                paymentMethod="ETH"
-                signer={{
-                    address: "0xdC9bb9929b79b62d630A7C3568c979a2843eFd8b",
-                    signAndSendTransaction: async (tx) => {
-                        return "0x1234";
-                    },
-                }}
-                onEvent={(event) => {
-                    console.log(event);
-                }}
-            />
-        </div>
+            >
+                <button onClick={() => setCount(count + 1)}>Increment count: {count}</button>
+
+                <DynamicConnectButton>
+                    <p>Connect</p>
+                </DynamicConnectButton>
+
+                {<Content count={count} />}
+            </div>
+        </DynamicContextProvider>
+    );
+}
+
+function Content({ count }: { count: number }) {
+    const [signer, setSigner] = useState<any>(null);
+
+    const { walletConnector } = useDynamicContext();
+
+    async function getSigner() {
+        const _signer = await walletConnector?.getSigner();
+        console.log("_signer", _signer);
+        setSigner(_signer);
+    }
+
+    useEffect(() => {
+        console.log("walletConnector", walletConnector);
+        getSigner();
+    }, [walletConnector]);
+
+    if (signer == null) {
+        return <p>Connect wallet</p>;
+    }
+
+    return (
+        <CrossmintPaymentElement
+            environment="https://crossmint-main-git-checkout-embedded-p5-crossmint.vercel.app"
+            clientId="db218e78-d042-4761-83af-3c4e5e6659dd"
+            recipient={{ wallet: "maxfQWBno84Zfu4sXgmjYvsvLn4LzGFSgSkFMFuzved" }}
+            mintConfig={{
+                testCount: count,
+            }}
+            paymentMethod="SOL"
+            signer={{
+                address: signer.publicKey.toString(),
+                signAndSendTransaction: signer.signAndSendTransaction.bind(signer),
+            }}
+            onEvent={(event) => {
+                console.log(event);
+
+                if (event.type === "payment:process.succeeded") {
+                    console.log("PAYMENT SUCCESS. SHOW MINTING", event);
+                }
+            }}
+        />
     );
 }
