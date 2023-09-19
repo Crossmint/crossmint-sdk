52bc693f28251e1f04da8ad6d6c58f0968375a75
Maxwell Fortney <maxwell@paella.dev>
2023-09-19 11:14:02 -0400

    v1.1: json types and updatable params

---
+++ packages/core/base/src/types/embed/json/index.ts
@@ -0,0 +1,41 @@
+import {
+    CaseInsensitive,
+    CollectionOrClientId,
+    Currency,
+    Locale,
+    MintConfigs,
+    PaymentMethod,
+    Recipient,
+    UIConfig,
+} from "../..";
+import { CryptoEmbeddedCheckoutPropsJSONParsed, CryptoEmbeddedCheckoutPropsJSONStringified } from "./crypto";
+import { FiatEmbeddedCheckoutPropsJSONParsed, FiatEmbeddedCheckoutPropsJSONStringified } from "./fiat";
+
+export type CommonEmbeddedCheckoutPropsJSONStringified<PM extends PaymentMethod = PaymentMethod> = {
+    paymentMethod?: PM;
+    mintConfig?: string;
+    currency?: CaseInsensitive<Currency>;
+    locale?: Locale;
+    uiConfig?: string;
+    whPassThroughArgs?: string;
+    projectId?: string;
+    recipient?: string;
+} & CollectionOrClientId;
+
+export type CommonEmbeddedCheckoutPropsJSONParsed<PM extends PaymentMethod = PaymentMethod> = {
+    paymentMethod?: PM;
+    mintConfig?: MintConfigs;
+    currency?: CaseInsensitive<Currency>;
+    locale?: Locale;
+    uiConfig?: UIConfig;
+    whPassThroughArgs?: any;
+    projectId?: string;
+    recipient?: Recipient;
+} & CollectionOrClientId;
+
+export type CrossmintEmbeddedCheckoutPropsJSONStringified =
+    | FiatEmbeddedCheckoutPropsJSONStringified
+    | CryptoEmbeddedCheckoutPropsJSONStringified;
+export type CrossmintEmbeddedCheckoutPropsJSONParsed =
+    | FiatEmbeddedCheckoutPropsJSONParsed
+    | CryptoEmbeddedCheckoutPropsJSONParsed;
