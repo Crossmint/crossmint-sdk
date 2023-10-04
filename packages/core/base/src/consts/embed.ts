83880815bcedc328d15147eb805b5cec36cefcb6
guillermodoghel <guillermo@paella.dev>
2023-10-04 17:55:51 -0300

    Adds support for multi language and multi currency on old fiat embedded checkout.

--- packages/core/base/src/consts/embed.ts
+++ packages/core/base/src/consts/embed.ts
@@ -1,5 +1,5 @@
 export const embeddedCheckoutIFrameId = "crossmint-embedded-checkout.iframe";
 
-export const updatableCommonParams = ["recipient", "mintConfig", "locale", "curreny", "whPassThroughArgs"] as const;
+export const updatableCommonParams = ["recipient", "mintConfig", "locale", "currency", "whPassThroughArgs"] as const;
 export const updatableFiatParams = [...updatableCommonParams] as const;
 export const updatableCryptoParams = ["signer", ...updatableCommonParams] as const;
