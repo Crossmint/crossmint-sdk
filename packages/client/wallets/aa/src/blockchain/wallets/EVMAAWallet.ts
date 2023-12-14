505519dace6154be0940d1524f9f20b115ba3540
matinavdev <matias@paella.dev>
2023-12-14 15:55:23 -0300

    remove unnecessary changes

--- packages/client/wallets/aa/src/blockchain/wallets/EVMAAWallet.ts
+++ packages/client/wallets/aa/src/blockchain/wallets/EVMAAWallet.ts
@@ -14,12 +14,12 @@ import { getFunctionSelector } from "viem";
 import { CrossmintService } from "../../api/CrossmintService";
 import { GenerateSignatureDataInput } from "../../types/API";
 import { ZERO_PROJECT_ID } from "../../utils/constants";
-import { Blockchain, getBlockchainByChainId, getUrlProviderByBlockchain } from "../BlockchainNetworks";
+import { EVMBlockchain, getBlockchainByChainId, getUrlProviderByBlockchain } from "../BlockchainNetworks";
 import { Custodian } from "../plugins";
 import { TokenType } from "../token/Tokens";
 import BaseWallet from "./BaseWallet";
 
-export class EVMAAWallet<B extends Blockchain = Blockchain> extends BaseWallet {
+export class EVMAAWallet<B extends EVMBlockchain = EVMBlockchain> extends BaseWallet {
     private sessionKeySignerAddress?: string;
     chain: B;
 
