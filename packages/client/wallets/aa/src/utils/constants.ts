35aff5a395559ced2025942c5fcc11150c099846
matinavdev <matias@paella.dev>
2023-12-18 12:25:15 -0300

    remove unnecessary change

--- packages/client/wallets/aa/src/utils/constants.ts
+++ packages/client/wallets/aa/src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const ZERO_PROJECT_ID = "7016355c-c7b3-4d57-a50e-84e9d02d9c52";
 export const ZERO_DEV_TYPE = "ZeroDev";
 export const CURRENT_VERSION = 0;
-export const CROSSMINT_STG_URL = "http://localhost:3000/api";
+export const CROSSMINT_STG_URL = "https://staging.crossmint.com/api";
 export const CROSSMINT_PROD_URL = "https://crossmint.com/api";
 export const WEB3_AUTH_TESTNET = "testnet";
 export const WEB3_AUTH_MAINNET = "mainnet";
