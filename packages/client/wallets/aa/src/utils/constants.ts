378d0d3a6db7bf13f97bd99716763600adef2330
Maximiliano Schultheis <max@paella.dev>
2023-12-19 17:06:02 -0300

    update project id

--- packages/client/wallets/aa/src/utils/constants.ts
+++ packages/client/wallets/aa/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const ZERO_PROJECT_ID = "7016355c-c7b3-4d57-a50e-84e9d02d9c52";
+export const ZERO_PROJECT_ID = "9a334a4d-64d4-465c-ad81-856c6129e064";
 export const ZERO_DEV_TYPE = "ZeroDev";
 export const CURRENT_VERSION = 0;
 export const CROSSMINT_STG_URL = "https://staging.crossmint.com/api";
