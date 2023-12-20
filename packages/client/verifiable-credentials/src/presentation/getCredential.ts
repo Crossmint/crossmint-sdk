a1821113edfe56bb7a335b6adcf268d714592ef5
Valerio Massimo <140358313+vmc-crossmint@users.noreply.github.com>
2023-12-20 16:28:41 +0100

    Credentials: Add retrieval and decryption (#301)
    * Decryption
    
    * lock
    * fix
    
    * update api
    
    * update to api key

---
+++ packages/client/verifiable-credentials/src/presentation/getCredential.ts
@@ -0,0 +1,28 @@
+import { VerifiableCredential } from "@/types/verifiableCredential";
+
+import { getEnvironmentBaseUrl } from "@crossmint/client-sdk-base";
+
+import { CrossmintAPI } from "../services/crossmintAPI";
+
+export async function getCredentialFromId(credentialId: string, environment: string) {
+    const baseUrl = getEnvironmentBaseUrl(environment);
+    const headers = CrossmintAPI.getHeaders();
+
+    console.debug(`Fetching credential ${credentialId}`);
+    const url = `${baseUrl}/api/vc-alpha1/credentials/${credentialId}`;
+    const options = { method: "GET", headers: headers };
+    console.log(url, options);
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(
+                `HTTP error! status: ${response.status}, responses: ${response.statusText}, ${response.body}`
+            );
+        }
+        const credential = (await response.json()) as VerifiableCredential;
+        return credential;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
--- packages/client/verifiable-credentials/src/services/crossmintAPI.ts
+++ packages/client/verifiable-credentials/src/services/crossmintAPI.ts
@@ -1,20 +1,17 @@
 export class CrossmintAPI {
-    private static projectID: string;
-    private static clientSecret: string;
+    private static apiKey: string;
 
-    public static setCredentials(projectID: string, clientSecret: string) {
-        this.projectID = projectID;
-        this.clientSecret = clientSecret;
+    public static init(apiKey: string) {
+        this.apiKey = apiKey;
     }
 
     public static getHeaders() {
-        if (!this.projectID || !this.clientSecret) {
+        if (!this.apiKey) {
             throw new Error("Credentials not set");
         }
 
         return {
-            "x-project-id": this.projectID,
-            "x-client-secret": this.clientSecret,
+            "x-api-key": this.apiKey,
             accept: "application/json",
         };
     }
