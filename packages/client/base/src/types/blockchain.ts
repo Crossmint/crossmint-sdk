0438a28e52c8e8a6e5110594fdcde18ee1a6289b
Maximiliano Schultheis <max@paella.dev>
2024-01-04 17:25:32 -0300

    moved blockchains to base

--- packages/client/base/src/types/blockchain.ts
+++ packages/client/base/src/types/blockchain.ts
@@ -1,9 +1,30 @@
 import { UIConfig } from "./uiconfig";
 
-export const EVM_CHAINS = ["ethereum", "polygon", "bsc"] as const;
-export const ALL_CHAINS = ["solana", "cardano", ...EVM_CHAINS] as const;
-export type EVMChain = (typeof EVM_CHAINS)[number];
+export const EVM_CHAINS = [
+    "ethereum",
+    "ethereum-sepolia",
+    "polygon",
+    "bsc",
+    "optimism",
+    "arbitrum",
+    "base",
+    "zora",
+    "arbitrumnova",
+    "zkatana",
+] as const;
+
+export const ALL_CHAINS = ["solana", "cardano", "sui", ...EVM_CHAINS] as const;
 export type Blockchain = (typeof ALL_CHAINS)[number];
+
+export type EVMBlockchain = (typeof EVM_CHAINS)[number];
+export const BLOCKCHAIN_TEST_NET = ["goerli", "ethereum-sepolia", "mumbai", "zkatana"] as const;
+export type BlockchainTestNet = (typeof BLOCKCHAIN_TEST_NET)[number];
+
+export const EVM_BLOCKCHAIN_WITH_TESTNET = [...EVM_CHAINS, ...BLOCKCHAIN_TEST_NET] as const;
+export type EVMBlockchainWithTestnet = (typeof EVM_BLOCKCHAIN_WITH_TESTNET)[number];
+
+export const ALL_BLOCKCHAIN_WITH_TESTNET = [...ALL_CHAINS, ...BLOCKCHAIN_TEST_NET] as const;
+export type AllBlockchainWithTestnet = (typeof ALL_BLOCKCHAIN_WITH_TESTNET)[number];
 
 export interface Wallet {
     chain: string;
@@ -11,7 +32,7 @@ export interface Wallet {
 }
 
 export interface EVMNFT {
-    chain: EVMChain;
+    chain: EVMBlockchain;
     contractAddress: string;
     tokenId: string;
 }
@@ -26,7 +47,7 @@ export interface CardanoNFT {
     assetId: string;
 }
 
-export type NFTLocator<T extends Blockchain> = `${T}:${string}${T extends EVMChain ? `:${string}` : ""}`;
+export type NFTLocator<T extends Blockchain> = `${T}:${string}${T extends EVMBlockchain ? `:${string}` : ""}`;
 
 export type NFT = SolanaNFT | EVMNFT | CardanoNFT;
 
