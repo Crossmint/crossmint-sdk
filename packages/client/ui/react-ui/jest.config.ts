5a06021b403691563e6ac11e00ae87a0a8d2731a
Austin Feight <austin@paella.dev>
2024-01-11 10:32:25 -0500

    test

--- packages/client/ui/react-ui/jest.config.ts
+++ packages/client/ui/react-ui/jest.config.ts
@@ -1,8 +1,11 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
     preset: "ts-jest",
     testEnvironment: "jest-environment-jsdom",
     moduleNameMapper: {
         "^@/(.*)$": "<rootDir>/src/$1",
     },
 };
+
+export default jestConfig;
