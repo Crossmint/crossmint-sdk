8d175067572d921fc0c1917cdbc81ca330b6cc44
MatiNavCrossmint <matias@paella.dev>
2024-01-12 10:45:18 -0300

    NCW: Add Datadog (#336)
    NCW: Add Datadog (#336)

--- packages/client/wallets/aa/src/services/logging/index.ts
+++ packages/client/wallets/aa/src/services/logging/index.ts
@@ -1,8 +1,14 @@
 
+import { isLocalhost } from "@/utils/helpers";
 import { ConsoleProvider } from "./ConsoleProvider";
+import { DatadogProvider } from "./DatadogProvider";
 
 function getBrowserLogger() {
-    return new ConsoleProvider();
+    if (isLocalhost()) {
+        return new ConsoleProvider();
+    }
+
+    return new DatadogProvider();
 }
 
 const { logInfo, logWarn, logError } = getBrowserLogger();
