267303546bda698413118cc7055d0b4252bac14d
Austin Feight <austin@paella.dev>
2024-02-01 09:43:01 -0500

    make localstorage user-unique to avoid collision

--- packages/client/wallets/aa/src/blockchain/wallets/FireblocksNCWallet.ts
+++ packages/client/wallets/aa/src/blockchain/wallets/FireblocksNCWallet.ts
@@ -18,17 +18,24 @@ import { PasswordEncryptedLocalStorage } from "../../storage/PasswordEncryptedLo
 import { KeysGenerationError, NonCustodialWalletError, SignTransactionError } from "../../utils/error";
 import { getFireblocksAssetId } from "../BlockchainNetworks";
 
-export const FireblocksNCWallet = async (
-    userIdentifier: UserIdentifier,
-    crossmintService: CrossmintService,
-    chain: BlockchainIncludingTestnet,
-    passphrase: string,
-    ncwData?: {
-        walletId: string;
-        deviceId: string;
-    }
-) => {
-    const localStorageRepository = new LocalStorageRepository();
+type FireblocksNCWWalletInput = {
+    userIdentifier: UserIdentifier;
+    projectId: string;
+    crossmintService: CrossmintService;
+    chain: BlockchainIncludingTestnet;
+    passphrase: string;
+    ncwData?: { walletId: string; deviceId: string };
+};
+
+export const FireblocksNCWallet = async ({
+    userIdentifier,
+    projectId,
+    crossmintService,
+    chain,
+    passphrase,
+    ncwData,
+}: FireblocksNCWWalletInput) => {
+    const localStorageRepository = new LocalStorageRepository(userIdentifierString(userIdentifier), projectId);
 
     let _walletId: string;
     let _deviceId: string;
@@ -204,3 +211,14 @@ const getDefaultAlgorithems = (): Set<TMPCAlgorithm> => {
     algorithms.add("MPC_CMP_ECDSA_SECP256K1");
     return algorithms;
 };
+
+function userIdentifierString(userIdentifier: UserIdentifier) {
+    switch (userIdentifier.type) {
+        case "email":
+            return `email:${userIdentifier.email}`;
+        case "phoneNumber":
+            return `phoneNumber:${userIdentifier.phoneNumber}`;
+        case "whiteLabel":
+            return `userId:${userIdentifier.userId}`;
+    }
+}
--- packages/client/wallets/aa/src/storage/LocalStorageRepository.ts
+++ packages/client/wallets/aa/src/storage/LocalStorageRepository.ts
@@ -1,28 +1,37 @@
 export class LocalStorageRepository {
-    private NCW_DEVICE_ID_KEY = "NCW-deviceId";
-    private NCW_WALLET_ID_KEY = "NCW-walletId";
-    private NCW_ADDRESS_KEY = "NCW-address";
+    private NCW_DEVICE_ID_KEY_PREFIX = "NCW-deviceId";
+    private NCW_WALLET_ID_KEY_PREFIX = "NCW-walletId";
+    private NCW_ADDRESS_KEY_PREFIX = "NCW-address";
+
+    constructor(private readonly userIdentifier: string, private readonly projectId: string) {}
 
     get ncwData() {
-        const deviceId = localStorage.getItem(this.NCW_DEVICE_ID_KEY);
-        const walletId = localStorage.getItem(this.NCW_WALLET_ID_KEY);
+        const deviceId = localStorage.getItem(this.key(this.NCW_DEVICE_ID_KEY_PREFIX));
+        const walletId = localStorage.getItem(this.key(this.NCW_WALLET_ID_KEY_PREFIX));
         return deviceId && walletId ? { walletId, deviceId } : null;
     }
 
     set ncwData(data: { walletId: string; deviceId: string } | null) {
-        if (!data) {
+        if (data == null) {
             return;
         }
+
         const { walletId, deviceId } = data;
-        localStorage.setItem(this.NCW_DEVICE_ID_KEY, deviceId);
-        localStorage.setItem(this.NCW_WALLET_ID_KEY, walletId);
+        localStorage.setItem(this.key(this.NCW_DEVICE_ID_KEY_PREFIX), deviceId);
+        localStorage.setItem(this.key(this.NCW_WALLET_ID_KEY_PREFIX), walletId);
     }
 
     get ncwAddress() {
-        return localStorage.getItem(this.NCW_ADDRESS_KEY);
+        return localStorage.getItem(this.key(this.NCW_ADDRESS_KEY_PREFIX));
     }
 
     set ncwAddress(address: string | null) {
-        address ? localStorage.setItem(this.NCW_ADDRESS_KEY, address) : localStorage.removeItem(this.NCW_ADDRESS_KEY);
+        address
+            ? localStorage.setItem(this.key(this.NCW_ADDRESS_KEY_PREFIX), address)
+            : localStorage.removeItem(this.key(this.NCW_ADDRESS_KEY_PREFIX));
+    }
+
+    private key(prefix: string) {
+        return `${prefix}-${this.userIdentifier}-${this.projectId}`;
     }
 }
