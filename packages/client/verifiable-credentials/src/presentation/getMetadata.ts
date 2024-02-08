32b87afcf2f8d1ce34ae69b05458268e389d310f
vmc-crossmint <valerio@paella.dev>
2024-02-09 00:04:14 +0100

    Allow different gateways

--- packages/client/verifiable-credentials/src/presentation/getMetadata.ts
+++ packages/client/verifiable-credentials/src/presentation/getMetadata.ts
@@ -1,32 +1,50 @@
+import { CrossmintAPI } from "@/services/crossmintAPI";
 import { ethers, utils } from "ethers";
 
 import { nftZeroEight } from "../ABI/upgradeable721-v0.8";
 import { getProvider } from "../services/provider";
 import { CredentialsCollection } from "../types/nfts";
 
-const ipfsGateway = "https://ipfs.io/ipfs/";
-
 export async function getMetadata(contractAddress: string, environment: string): Promise<any> {
     const ABI = new utils.Interface(nftZeroEight.abi);
     const provider = getProvider(environment);
     const contract = new ethers.Contract(contractAddress, ABI, provider);
+    const gateways = CrossmintAPI.ipfsGateways;
 
+    let uri;
     try {
-        const uri = await contract.contractURI();
-        if (uri == null) {
-            return null;
-        }
-
-        const httpUri = uri.replace("ipfs://", ipfsGateway);
-        const response = await fetch(httpUri);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}, responses: ${response.statusText}`);
-        }
-
-        const metadata = await response.json();
-        return metadata;
+        uri = await contract.contractURI();
     } catch (error) {
-        console.error(`Failed to get metadata for contract ${contractAddress}: ${error}`);
+        console.error(`Failed to get contractURI ${contractAddress}: ${error}`);
+        return null;
+    }
+    if (uri == null) {
+        return null;
+    }
+    console.debug(`Got contractURI ${uri} for contract ${contractAddress}`);
+
+    const httpUri = uri.replace("ipfs://", "");
+    for (const gateway of gateways) {
+        try {
+            const httpUriFull = formatUrl(gateway, httpUri);
+            const timeout = new Promise((resolve, reject) => {
+                const timeoutSeconds = 2;
+                const id = setTimeout(() => {
+                    clearTimeout(id);
+                    reject(`Timed out in ${timeoutSeconds} seconds`);
+                }, timeoutSeconds);
+            });
+
+            const response = (await Promise.race([fetch(httpUriFull), timeout])) as Response;
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}, responses: ${response.statusText}`);
+            }
+
+            const metadata = await response.json();
+            return metadata;
+        } catch (error) {
+            console.error(`Failed to get metadata for contract ${contractAddress} with gataway ${gateway}: ${error}`);
+        }
     }
 }
 
@@ -47,4 +65,8 @@ export async function getContractWithVCMetadata(
     }
 
     return credentialCollections;
+}
+
+function formatUrl(template: string, cid: string): string {
+    return template.replace("{cid}", cid);
 }
--- packages/client/verifiable-credentials/src/services/crossmintAPI.ts
+++ packages/client/verifiable-credentials/src/services/crossmintAPI.ts
@@ -1,8 +1,17 @@
 export class CrossmintAPI {
     private static apiKey: string;
+    public static ipfsGateways: string[];
 
-    public static init(apiKey: string) {
+    public static init(
+        apiKey: string,
+        ipfsGateways: string[] = [
+            "https://fleek.ipfs.io/ipfs/{cid}",
+            "https://ipfs.io/ipfs/{cid}",
+            "https://gateway.ipfs.io/ipfs/{cid}",
+        ]
+    ) {
         this.apiKey = apiKey;
+        this.ipfsGateways = ipfsGateways;
     }
 
     public static getHeaders() {
