679be59e54d9352a3d2dec6934176ade3acd0c15
Lluis Erena <lluis.bip@paella.dev>
2024-02-29 15:05:04 +0100

    SCW-391:Improve SDK Demo site (#401)

--- packages/client/wallets/aa/front/pages/index.tsx
+++ packages/client/wallets/aa/front/pages/index.tsx
@@ -1,9 +1,12 @@
 import {
-    getOrCreateWallet,
+    getOrCreateWalletEthers,
+    getOrCreateWalletFireblocks,
     sendTransaction,
     setCustodianForKillSwitch,
     setCustodianForTokens,
     signMessage,
+    signTypedData,
+    verifyMessage,
 } from "@/services/walletService";
 import { useState } from "react";
 
@@ -12,23 +15,48 @@ import { EVMAAWallet } from "@crossmint/client-sdk-aa";
 import styles from "../styles/index.module.css";
 
 export default function WalletPage() {
-    const [walletAddress, setWalletAddress] = useState("");
-    const [signMessageTxHash, setSignMessageTxHash] = useState("");
-    const [sendTransactionTxHash, setSendTransactionTxHash] = useState("");
-    const [aaWallet, setAAWallet] = useState<EVMAAWallet | undefined>();
-    const [email, setEmail] = useState<string>();
-    const [privateKey, setPrivateKey] = useState<string>();
+    const [messageToSign, setMessageToSign] = useState("");
+    const [signMessageSignature, setSignMessageSignature] = useState("");
 
-    const handleCreateWallet = async () => {
+    const [messageToVerify, setMessageToVerify] = useState("");
+    const [messageSignature, setMessageSignature] = useState("");
+    const [isMessageVerified, setIsMessageVerified] = useState(false);
+
+    const [signTypedDataHash, setSignTypedDataHash] = useState("");
+
+    const [sendTransactionTxHash, setSendTransactionTxHash] = useState("");
+
+    const [aaWallet, setAAWallet] = useState<EVMAAWallet | undefined>();
+
+    const [email, setEmail] = useState("");
+    const [privateKey, setPrivateKey] = useState("");
+    const [walletAddress, setWalletAddress] = useState("");
+
+    const [emailFireblocks, setEmailFireblocks] = useState("");
+    const [walletAddressFireblocks, setWalletAddressFireblocks] = useState("");
+
+    const handleCreateWalletEthers = async () => {
         if (!email || !privateKey) {
             console.log("Input 'email' and 'privateKey' values");
             return;
         }
         console.log("Creating wallet");
-        const aaWallet = await getOrCreateWallet(email, privateKey);
+        const aaWallet = await getOrCreateWalletEthers(email, privateKey);
         console.log({ aaWallet });
         setAAWallet(aaWallet);
         setWalletAddress(await aaWallet.getAddress());
+    };
+
+    const handleCreateWalletFireblocks = async () => {
+        if (!emailFireblocks) {
+            console.log("Input 'email' value");
+            return;
+        }
+        console.log("Creating wallet");
+        const aaWallet = await getOrCreateWalletFireblocks(emailFireblocks);
+        console.log({ aaWallet });
+        setAAWallet(aaWallet);
+        setWalletAddressFireblocks(await aaWallet.getAddress());
     };
 
     const handleSendTransaction = async () => {
@@ -45,11 +73,49 @@ export default function WalletPage() {
         }
     };
 
+    const handleSignTypedData = async () => {
+        if (aaWallet) {
+            const params = {
+                primaryType: "ValidatorApproved",
+                message: {
+                    sig: "0x1888bfd7",
+                    validatorData: "1286154452161992304311869645070421180087210148582",
+                    executor: "0x2087C7FfD0d0DAE80a00EE74325aBF3449e0eaf1",
+                    enableData:
+                        "0x902588bbC55727dAa4224f5D6056ba939534efcD80ac58cd1888bfd700000000000000000000000000000010",
+                },
+                types: {
+                    ValidatorApproved: [
+                        { name: "sig", type: "bytes4" },
+                        { name: "validatorData", type: "uint256" },
+                        { name: "executor", type: "address" },
+                        { name: "enableData", type: "bytes" },
+                    ],
+                },
+            };
+            const txHash = await signTypedData(aaWallet, params);
+            console.log({ signTypedDataHash: txHash });
+            setSignTypedDataHash(txHash);
+        } else {
+            console.log("AAWallet not defined");
+        }
+    };
+
     const handleSignMessage = async () => {
         if (aaWallet) {
-            const tx = await signMessage(aaWallet, "TEST_MESSAGE");
+            const tx = await signMessage(aaWallet, messageToSign);
             console.log({ signMessage_txHash: tx });
-            setSignMessageTxHash(tx);
+            setSignMessageSignature(tx);
+        } else {
+            console.log("AAWallet not defined");
+        }
+    };
+
+    const handleVerifyMessage = async () => {
+        if (aaWallet) {
+            const verified = await verifyMessage(aaWallet, messageToVerify, messageSignature);
+            console.log({ verified });
+            setIsMessageVerified(verified);
         } else {
             console.log("AAWallet not defined");
         }
@@ -66,49 +132,143 @@ export default function WalletPage() {
     };
 
     return (
-        <div className={styles.container}>
-            <section className={styles.section}>
-                <h2 className={styles.title}>Get / Create</h2>
-                <input
-                    className={styles.input}
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    className={styles.input}
-                    placeholder="Private key"
-                    value={privateKey}
-                    onChange={(e) => setPrivateKey(e.target.value)}
-                />
-                <button className={styles.button} onClick={handleCreateWallet}>
-                    Create wallet
-                </button>
-                <input className={styles.input} type="text" value={walletAddress} data-testid="createdOrGotWallet" readOnly />
-            </section>
+        <div>
+            <div className={styles.container}>
+                <section className={styles.section}>
+                    <h2 className={styles.title}>Get / Create</h2>
+                    <div>
+                        <h3 className={styles.title}>Ethers</h3>
+                        <input
+                            className={styles.input}
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            className={styles.input}
+                            placeholder="Private key"
+                            value={privateKey}
+                            onChange={(e) => setPrivateKey(e.target.value)}
+                        />
+                        <button className={styles.button} onClick={handleCreateWalletEthers}>
+                            Create wallet
+                        </button>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={walletAddress}
+                            data-testid="createdOrGotWallet"
+                            readOnly
+                        />
+                    </div>
+                    <div>
+                        <h3 className={styles.title}>Fireblocks</h3>
+                        <input
+                            className={styles.input}
+                            type="email"
+                            placeholder="Email"
+                            value={emailFireblocks}
+                            onChange={(e) => setEmailFireblocks(e.target.value)}
+                        />
+                        <button className={styles.button} onClick={handleCreateWalletFireblocks}>
+                            Create wallet
+                        </button>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={walletAddressFireblocks}
+                            data-testid="createdOrGotWallet"
+                            readOnly
+                        />
+                    </div>
+                </section>
+            </div>
 
-            <section className={styles.section}>
-                <h2 className={styles.title}>Custodians</h2>
-                <button className={styles.button} onClick={handleSetCustodianKillSwitch}>
-                    KillSwitch
-                </button>
-                <button className={styles.button} onClick={handleSetCustodianTransfer}>
-                    Tokens
-                </button>
-            </section>
+            <div className={styles.container}>
+                <section className={styles.section}>
+                    <h2 className={styles.title}>Custodians</h2>
+                    <button className={styles.button} onClick={handleSetCustodianKillSwitch}>
+                        KillSwitch
+                    </button>
+                    <button className={styles.button} onClick={handleSetCustodianTransfer}>
+                        Tokens
+                    </button>
+                </section>
+            </div>
 
-            <section className={styles.section}>
-                <h2 className={styles.title}>Wallet actions</h2>
-                <button className={styles.button} onClick={handleSignMessage}>
-                    Sign Message
-                </button>
-                <input className={styles.input} type="text" value={signMessageTxHash} data-testid="signedMessageInput" readOnly />
-                <button className={styles.button} onClick={handleSendTransaction}>
-                    Send transaction
-                </button>
-                <input className={styles.input} type="text" value={sendTransactionTxHash} data-testid="sendTransactionTxHashInput" readOnly />
-            </section>
+            <div className={styles.container}>
+                <section className={styles.section}>
+                    <h2 className={styles.title}>Wallet actions</h2>
+                    <div>
+                        <h3 className={styles.title}>Sign / Verify message</h3>
+                        <div>
+                            <input
+                                className={styles.input}
+                                type="message"
+                                placeholder="Message to sign"
+                                value={messageToSign}
+                                onChange={(e) => setMessageToSign(e.target.value)}
+                            />
+                            <button className={styles.button} onClick={handleSignMessage}>
+                                Sign Message
+                            </button>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                value={signMessageSignature}
+                                data-testid="signedMessageInput"
+                                readOnly
+                            />
+                        </div>
+                        <div>
+                            <input
+                                className={styles.input}
+                                type="messageVerify"
+                                placeholder="Message to verify"
+                                value={messageToVerify}
+                                onChange={(e) => setMessageToVerify(e.target.value)}
+                            />
+                            <input
+                                className={styles.input}
+                                type="signature"
+                                placeholder="Signature"
+                                value={messageSignature}
+                                onChange={(e) => setMessageSignature(e.target.value)}
+                            />
+                            <button className={styles.button} onClick={handleVerifyMessage}>
+                                Verify message
+                            </button>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                value={isMessageVerified ? "Verified" : "Not verified"}
+                                readOnly
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className={styles.title}>Sign Typed Data</h3>
+                        <button className={styles.button} onClick={handleSignTypedData}>
+                            Sign Typed Data
+                        </button>
+                        <input className={styles.input} type="text" value={signTypedDataHash} readOnly />
+                    </div>
+                    <div>
+                        <h3 className={styles.title}>Send transaction</h3>
+                        <button className={styles.button} onClick={handleSendTransaction}>
+                            Send transaction
+                        </button>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={sendTransactionTxHash}
+                            data-testid="sendTransactionTxHashInput"
+                            readOnly
+                        />
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
