f526adb9c0ca9f3488fd81ad9a0d2716529efe8d
Valerio Massimo <140358313+vmc-crossmint@users.noreply.github.com>
2024-03-20 09:46:44 -0400

    VerifiableCredentials: update RPCs (#442)
    * Updated polygon rpcs
    
    * version bump

--- packages/client/verifiable-credentials/src/services/provider.test.ts
+++ packages/client/verifiable-credentials/src/services/provider.test.ts
@@ -6,12 +6,12 @@ describe("getProvider", () => {
     it("returns the production provider for production environments", () => {
         const provider = getProvider("prod");
         expect(provider).toBeInstanceOf(StaticJsonRpcProvider);
-        expect(provider.connection.url).toBe("https://rpc-mainnet.maticvigil.com/");
+        expect(provider.connection.url).toBe("https://polygon.llamarpc.com/");
     });
 
     it("returns the test provider for non-production environments", () => {
         const provider = getProvider("dev");
         expect(provider).toBeInstanceOf(StaticJsonRpcProvider);
-        expect(provider.connection.url).toBe("https://rpc-mumbai.maticvigil.com/");
+        expect(provider.connection.url).toBe("https://polygon-mumbai-pokt.nodies.app/");
     });
 });
