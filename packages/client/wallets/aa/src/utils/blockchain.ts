9cd71f211abd28e41e6f9fb0372cfebeede42d13
matinavdev <matias@paella.dev>
2024-04-25 16:19:14 -0300

    add verify messsage function

--- packages/client/wallets/aa/src/utils/blockchain.ts
+++ packages/client/wallets/aa/src/utils/blockchain.ts
@@ -1,7 +1,18 @@
+import { getBundlerRPC } from "@/blockchain";
 import type { Deferrable } from "@ethersproject/properties";
 import { type TransactionRequest } from "@ethersproject/providers";
 import { BigNumber, BigNumberish } from "ethers";
 import { BytesLike, hexlify } from "ethers/lib/utils";
+import { createPublicClient, http } from "viem";
+
+import { EVMBlockchainIncludingTestnet } from "@crossmint/common-sdk-base";
+
+export type VerifyMessageParams = {
+    address: `0x${string}`;
+    message: string;
+    signature: `0x${string}` | Uint8Array;
+    chain: EVMBlockchainIncludingTestnet;
+};
 
 /**
  * We need to support odd value and data fields in a tx, as that data comes from WC
@@ -50,4 +61,16 @@ export async function convertData(
     const resolvedData = await Promise.resolve(data);
     if (resolvedData === undefined) return undefined;
     return hexlify(resolvedData) as `0x${string}`;
+}
+
+export async function verifyMessage({ address, message, signature, chain }: VerifyMessageParams) {
+    const publicClient = createPublicClient({
+        transport: http(getBundlerRPC(chain)),
+    });
+
+    return publicClient.verifyMessage({
+        address,
+        message,
+        signature,
+    });
 }
