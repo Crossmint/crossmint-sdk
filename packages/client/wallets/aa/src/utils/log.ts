0a2fa622748bbaf584abd2195b36a4d2f0cec64c
matinavdev <matias@paella.dev>
2024-05-17 16:12:36 -0300

    improvement

--- packages/client/wallets/aa/src/utils/log.ts
+++ packages/client/wallets/aa/src/utils/log.ts
@@ -1,9 +1,14 @@
 import { logError, logInfo } from "@/services/logging";
+import { v4 as uuidv4 } from "uuid";
 
 import { SCW_SERVICE } from "./constants";
 
 export class LoggerWrapper {
+    private logIdempotencyKey: string;
+
     constructor(className: string, private extraInfo = {}) {
+        this.logIdempotencyKey = uuidv4();
+
         return new Proxy(this, {
             get: (target: any, propKey: PropertyKey, receiver: any) => {
                 const origMethod = target[propKey];
@@ -11,34 +16,21 @@ export class LoggerWrapper {
 
                 if (typeof origMethod === "function") {
                     return (...args: any[]) => {
-                        logInfo(
-                            `${identifierTag} input - ${beautify(args)} - extra_info - ${beautify(extraInfo)}`,
-                            addCommonKeysToLog({ args, ...extraInfo })
-                        );
+                        this.logInput(args, identifierTag);
+
                         const result = origMethod.apply(target, args);
                         if (result instanceof Promise) {
                             return result
                                 .then((res: any) => {
-                                    logInfo(
-                                        `${identifierTag} output - ${beautify(res)} - extra_info - ${beautify(
-                                            extraInfo
-                                        )}`,
-                                        addCommonKeysToLog({ res, ...extraInfo })
-                                    );
+                                    this.logOutput(res, identifierTag);
                                     return res;
                                 })
                                 .catch((err: any) => {
-                                    logError(
-                                        `${identifierTag} threw_error - ${err} - extra_info - ${beautify(extraInfo)}`,
-                                        addCommonKeysToLog({ err, ...extraInfo })
-                                    );
+                                    this.logError(err, identifierTag);
                                     throw err;
                                 });
                         } else {
-                            logInfo(
-                                `${identifierTag} output - ${beautify(result)} - extra_info - ${beautify(extraInfo)}`,
-                                addCommonKeysToLog({ res: result, ...extraInfo })
-                            );
+                            this.logOutput(result, identifierTag);
                             return result;
                         }
                     };
@@ -48,7 +40,38 @@ export class LoggerWrapper {
         });
     }
 
-    logPerformance<T>(name: string, cb: () => Promise<T>) {
+    private logInput(args: object, identifierTag: string) {
+        logInfo(
+            `${identifierTag} input - ${beautify(args)} - extra_info - ${beautify(
+                this.extraInfo
+            )} - log_idempotency_key - ${this.logIdempotencyKey}`,
+            addCommonKeysToLog({ args, ...this.extraInfo, logIdempotencyKey: this.logIdempotencyKey })
+        );
+    }
+
+    private logOutput(res: object, identifierTag: string) {
+        logInfo(
+            `${identifierTag} output - ${beautify(res)} - extra_info - ${beautify(
+                this.extraInfo
+            )} - log_idempotency_key - ${this.logIdempotencyKey}`,
+            addCommonKeysToLog({
+                res,
+                ...this.extraInfo,
+                logIdempotencyKey: this.logIdempotencyKey,
+            })
+        );
+    }
+
+    private logError(err: object, identifierTag: string) {
+        logError(
+            `${identifierTag} threw_error - ${err} - extra_info - ${beautify(this.extraInfo)} - log_idempotency_key - ${
+                this.logIdempotencyKey
+            }`,
+            addCommonKeysToLog({ err, ...this.extraInfo })
+        );
+    }
+
+    protected logPerformance<T>(name: string, cb: () => Promise<T>) {
         return logPerformance(name, cb, this.extraInfo);
     }
 }
@@ -62,13 +85,13 @@ export async function logPerformance<T>(name: string, cb: () => Promise<T>, extr
     return result;
 }
 
-function beautify(json: any) {
-    return json != null ? JSON.stringify(json, null, 2) : json;
-}
-
 export function addCommonKeysToLog(obj: any) {
     return {
         ...obj,
         service: SCW_SERVICE,
     };
+}
+
+function beautify(json: any) {
+    return json != null ? JSON.stringify(json, null, 2) : json;
 }
