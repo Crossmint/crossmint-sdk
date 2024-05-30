9e95c660469c55476d9456493e7c19411c041160
Devlyn Dorfer <devlyndorfer@gmail.com>
2024-05-30 19:27:31 -0400

    add good comment

--- packages/client/wallets/aa/src/blockchain/wallets/EVMAAWallet.ts
+++ packages/client/wallets/aa/src/blockchain/wallets/EVMAAWallet.ts
@@ -17,14 +17,7 @@ import {
     createZeroDevPaymasterClient,
 } from "@zerodev/sdk";
 import { EntryPoint } from "permissionless/types/entrypoint";
-import type {
-    Hash,
-    HttpTransport,
-    PublicClient,
-    SendTransactionParameters,
-    TransactionReceipt,
-    TypedDataDefinition,
-} from "viem";
+import type { Hash, HttpTransport, PublicClient, TransactionReceipt, TypedDataDefinition } from "viem";
 import { Chain, http, isAddress, isHex, publicActions } from "viem";
 
 import { EVMBlockchainIncludingTestnet } from "@crossmint/common-sdk-base";
@@ -115,34 +108,35 @@ export class EVMAAWallet extends LoggerWrapper {
         });
     }
 
-    public async sendTransaction({
-        to,
-        value,
-        data,
-    }: {
-        to: string;
-        value: bigint;
-        data?: string;
-    }): Promise<TransactionReceipt> {
+    /**
+     * Sends a transaction, waits for its completion, then returns the receipt.
+     * This function will validate the recipient address and the data format before sending the transaction.
+     *
+     * @param {string} to - The recipient's EVM (Ethereum Virtual Machine) compatible address.
+     * @param {bigint} value - The amount of cryptocurrency (in wei, where 1 ether = 10^18 wei) to send.
+     * @param {string} [data] - The hexadecimal string representing the data to be sent with the transaction.
+     * @returns {Promise<TransactionReceipt>} A promise that resolves to the transaction receipt object.
+     */
+    public async sendTransaction(to: string, value: bigint, data?: string): Promise<TransactionReceipt> {
         return this.logPerformance("SEND_TRANSACTION", async () => {
             if (!isAddress(to)) {
-                throw new Error("Invalid recipient address: '${to}' is not a valid EVM address.");
+                throw new Error(`Invalid recipient address: '${to}' is not a valid EVM address.`);
             }
 
             if (data != null && !isHex(data)) {
-                throw new Error("Invalid Hex: '${data}' is not valid Hex address.");
+                throw new Error(`Invalid Hex: '${data}' is not valid Hex data.`);
             }
 
             try {
-                const txParams: SendTransactionParameters<Chain, KernelSmartAccount<EntryPoint, HttpTransport>> = {
+                const tx = {
                     to,
                     value,
                     data,
                     ...(usesGelatoBundler(this.chain) && gelatoBundlerProperties),
                 };
 
-                logInfo(`[EVMAAWallet - SEND_TRANSACTION] - tx_params: ${JSON.stringify(txParams)}`);
-                const hash = await this.kernelClient.sendTransaction(txParams);
+                logInfo(`[EVMAAWallet - SEND_TRANSACTION] - tx_params: ${JSON.stringify(tx)}`);
+                const hash = await this.kernelClient.sendTransaction(tx);
                 return this.publicClient.waitForTransactionReceipt({ hash });
             } catch (error) {
                 throw new TransactionError(`Error sending transaction: ${error}`);
