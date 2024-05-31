de5b4bef84a07b35919c08bf5f8d1a5b65071125
Devlyn Dorfer <devlyndorfer@gmail.com>
2024-05-31 13:07:18 -0400

    cleanup reservoir signing function

--- packages/client/wallets/aa/src/adapters/reservoir.ts
+++ packages/client/wallets/aa/src/adapters/reservoir.ts
@@ -7,24 +7,26 @@ import { EVMAAWallet } from "../blockchain/wallets/EVMAAWallet";
 export function reservoirAdapter(wallet: EVMAAWallet): ReservoirWallet {
     return {
         address: async () => wallet.getAddress(),
-        handleSignMessageStep: async (stepItem, _) => {
-            const signData = stepItem.data?.sign;
-            let signature: string | undefined;
-            if (signData) {
-                if (signData.signatureKind === "eip191") {
-                    console.log("Execute Steps: Signing with eip191");
-                    signature = await wallet.signMessage(signData.message);
-                } else if (signData.signatureKind === "eip712") {
-                    console.log("Execute Steps: Signing with eip712");
-                    signature = await wallet.getSigner("viem").walletClient.signTypedData({
-                        domain: signData.domain as any,
-                        types: signData.types as any,
-                        primaryType: signData.primaryType,
-                        message: signData.value,
-                    });
-                }
+        handleSignMessageStep: async ({ data }, _) => {
+            const walletClient = wallet.getSigner("viem").walletClient;
+            const signData = data?.sign;
+
+            if (signData == null) {
+                return;
             }
-            return signature;
+
+            if (signData.signatureKind === "eip191") {
+                console.log("Execute Steps: Signing with eip191");
+                return walletClient.signMessage({ message: signData.message });
+            } else if (signData.signatureKind === "eip712") {
+                console.log("Execute Steps: Signing with eip712");
+                return walletClient.signTypedData({
+                    domain: signData.domain as any,
+                    types: signData.types as any,
+                    primaryType: signData.primaryType,
+                    message: signData.value,
+                });
+            }
         },
         handleSendTransactionStep: async (chainId, stepItem, _) => {
             const stepData = stepItem.data;
