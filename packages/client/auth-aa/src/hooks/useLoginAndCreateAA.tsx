394770ed1ed006a88e850a6bf41c5e892e042808
matinavdev <matias@paella.dev>
2024-06-13 11:26:31 -0300

    example updated

--- packages/client/auth-aa/src/hooks/useLoginAndCreateAA.tsx
+++ packages/client/auth-aa/src/hooks/useLoginAndCreateAA.tsx
@@ -1,28 +1,27 @@
 import { AuthProvider, useAuth } from '@crossmint/client-sdk-auth-core'
 
 export function useLoginAndCreateAA() {
-    const { login, user, sendApiEventToCrossmint } = useAuth();
+    const { login, user, fetchCrossmintAPI } = useAuth();
 
     async function createAAWallet() {
         if (!user) {
             throw new Error('User not logged in')
         }
 
-        const resp = await sendApiEventToCrossmint({
-            route: "/unstable/wallets/aa/wallets",
-            config: {
+        const resp = await fetchCrossmintAPI({
+            endpoint: "/unstable/wallets/aa/wallets",
+            options: {
                 method: "POST",
                 body: {
                     type: 'ZeroDev',
                     "smartContractWalletAddress": "0x7be8d6dD587E9EBB3e23da363144D2cF51776447",
                     "eoaAddress": "0x5dc8669b2DEE0392012CDb6867557b1C03FfA251",
-                    "sessionKeySignerAddress": "0xDb6CCe7b5b8534F797b29eb9e07aFd4bf9682e03",
                     version: 0,
                     baseLayer: 'evm',
                     chainId: 80002,
                     entryPointVersion: 'v0.7'
                 }
-            },
+            }
         })
         console.log(resp)
     }
