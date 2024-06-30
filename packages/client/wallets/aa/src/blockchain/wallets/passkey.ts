9f79d982667702676fdc74db7d7b7fd2f6d170df
Devlyn Dorfer <devlyndorfer@gmail.com>
2024-06-30 03:27:04 -0400

    copy over utils that can't be imported

--- packages/client/wallets/aa/src/blockchain/wallets/passkey.ts
+++ packages/client/wallets/aa/src/blockchain/wallets/passkey.ts
@@ -12,10 +12,9 @@ import {
 } from "@/index";
 import { WalletCreationParams } from "@/types/internal";
 import { createPasskeyValidator, deserializePasskeyValidator } from "@zerodev/passkey-validator";
-import { deserializePasskeyValidatorData, serializePasskeyValidatorData } from "@zerodev/passkey-validator/utils";
 import { KernelValidator, createKernelAccount } from "@zerodev/sdk";
 import { EntryPoint } from "permissionless/types/entrypoint";
-import { PublicClient } from "viem";
+import { Hex, PublicClient } from "viem";
 
 export interface PasskeyWalletParams extends WalletCreationParams {
     walletConfig: WalletConfig & { signer: PasskeySigner };
@@ -103,4 +102,44 @@ export class PasskeyWalletService {
             type: "passkeys",
         };
     }
+}
+
+type PasskeyValidatorSerializedData = {
+    passkeyServerUrl: string;
+    credentials: string;
+    entryPoint: Hex;
+    validatorAddress: Hex;
+    pubKeyX: string;
+    pubKeyY: string;
+    authenticatorIdHash: Hex;
+};
+
+const serializePasskeyValidatorData = (params: PasskeyValidatorSerializedData) => {
+    const replacer = (_: string, value: any) => {
+        if (typeof value === "bigint") {
+            return value.toString();
+        }
+        return value;
+    };
+
+    const jsonString = JSON.stringify(params, replacer);
+    const uint8Array = new TextEncoder().encode(jsonString);
+    const base64String = bytesToBase64(uint8Array);
+    return base64String;
+};
+
+export const deserializePasskeyValidatorData = (params: string) => {
+    const uint8Array = base64ToBytes(params);
+    const jsonString = new TextDecoder().decode(uint8Array);
+    return JSON.parse(jsonString) as PasskeyValidatorSerializedData;
+};
+
+function base64ToBytes(base64: string) {
+    const binString = atob(base64);
+    return Uint8Array.from(binString, (m) => m.codePointAt(0) as number);
+}
+
+function bytesToBase64(bytes: Uint8Array) {
+    const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join("");
+    return btoa(binString);
 }
