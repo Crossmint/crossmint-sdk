5a57332962517b644b4d4531cabd6b3e385a6376
Jonathan <jonathan.temp@paella.dev>
2024-07-20 13:05:22 -0700

    Smart wallet react starter (#566)
    * feat: Added React Starter to sdk
    
    * code cleanup, feedback
    
    * rename package and folder
    
    * update lock
    
    * fixed build
    
    * fixed test
    
    ---------
    
    Co-authored-by: Devlyn Dorfer <devlyndorfer@gmail.com>

---
+++ apps/wallets/smart-wallet/react/src/app/components/AuthenticatedRoute.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+
+import { AppContext } from "../AppContext";
+
+type AuthenticatedRouteProps = {
+    element: React.ReactElement;
+};
+
+export const AuthenticatedRoute = ({ element }: AuthenticatedRouteProps) => {
+    const { isAuthenticated } = useContext(AppContext);
+
+    if (isAuthenticated === null) {
+        return null;
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to="/" replace />;
+    }
+
+    return element;
+};
