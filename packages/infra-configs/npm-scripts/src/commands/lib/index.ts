7cad776375db2086ec8e8ba89fc0a2e3c1eb9944
Maxwell Fortney <maxwell@paella.dev>
2024-07-20 00:04:38 -0400

    tweak

--- packages/infra-configs/npm-scripts/src/commands/lib/index.ts
+++ packages/infra-configs/npm-scripts/src/commands/lib/index.ts
@@ -6,9 +6,6 @@ import {
 const libCommandToScriptMap: CommandToScriptOrCustomActionMap = {
     build: "cross-env NODE_OPTIONS='--max-semi-space-size=128 --max-old-space-size=8192' tsup",
     dev: "tsup --watch --clean false",
-    "test:vitest": "vitest run --passWithNoTests",
-    gen: "pnpm turbo gen new-library",
-    "test:tsc": "tsc --noEmit",
 };
 
 export const libCommand = commandFromCommandToScriptOrCustomActionMap("lib", libCommandToScriptMap);
