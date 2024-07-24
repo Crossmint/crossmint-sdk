bb1a2455ea9a0f5c84e98683b4d917bcc68d5ab7
jmderby <jonathan.temp@paella.dev>
2024-07-23 18:08:43 -0700

    added t3 zod wrapper

--- apps/wallets/smart-wallet/react/src/app/env.ts
+++ apps/wallets/smart-wallet/react/src/app/env.ts
@@ -1,37 +1,32 @@
+import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
-const envSchema = z.object({
-    REACT_APP_CROSSMINT_API_KEY_PROD: z.string(),
-    REACT_APP_CROSSMINT_API_KEY_STG: z.string().min(1),
-    REACT_APP_FIREBASE_API_KEY: z.string().min(1),
-    REACT_APP_FIREBASE_AUTH_DOMAIN: z.string().min(1),
-    REACT_APP_FIREBASE_PROJECT_ID: z.string().min(1),
-    REACT_APP_FIREBASE_STORAGE_BUCKET: z.string().min(1),
-    REACT_APP_FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
-    REACT_APP_FIREBASE_APP_ID: z.string().min(1),
-    REACT_APP_WEB3_AUTH_CLIENT_ID_PROD: z.string().min(1),
-    REACT_APP_WEB3_AUTH_CLIENT_ID_STG: z.string().min(1),
-    REACT_APP_WEB3_AUTH_NETWORK_PROD: z.string().min(1),
-    REACT_APP_WEB3_AUTH_NETWORK_STG: z.string().min(1),
-    REACT_APP_WEB3_AUTH_VERIFIER_ID_PROD: z.string().min(1),
-    REACT_APP_WEB3_AUTH_VERIFIER_ID_STG: z.string().min(1),
+export const env = createEnv({
+    server: {
+        /* add server .env here */
+    },
+
+    /**
+     * The prefix that client-side variables must have. This is enforced both at
+     * a type-level and at runtime.
+     */
+    clientPrefix: "REACT_APP_",
+    client: {
+        REACT_APP_CROSSMINT_API_KEY_PROD: z.string().optional(),
+        REACT_APP_CROSSMINT_API_KEY_STG: z.string().min(1),
+        REACT_APP_FIREBASE_API_KEY: z.string().min(1),
+        REACT_APP_FIREBASE_AUTH_DOMAIN: z.string().min(1),
+        REACT_APP_FIREBASE_PROJECT_ID: z.string().min(1),
+        REACT_APP_FIREBASE_STORAGE_BUCKET: z.string().min(1),
+        REACT_APP_FIREBASE_MESSAGING_SENDER_ID: z.string().min(1),
+        REACT_APP_FIREBASE_APP_ID: z.string().min(1),
+        REACT_APP_WEB3_AUTH_CLIENT_ID_PROD: z.string().optional(),
+        REACT_APP_WEB3_AUTH_CLIENT_ID_STG: z.string().min(1),
+        REACT_APP_WEB3_AUTH_NETWORK_PROD: z.string().optional(),
+        REACT_APP_WEB3_AUTH_NETWORK_STG: z.string().min(1),
+        REACT_APP_WEB3_AUTH_VERIFIER_ID_PROD: z.string().optional(),
+        REACT_APP_WEB3_AUTH_VERIFIER_ID_STG: z.string().min(1),
+    },
+    runtimeEnv: process.env,
+    emptyStringAsUndefined: true,
 });
-
-const envResult = envSchema.safeParse(process.env);
-
-function logEnvErrors(errors: z.ZodError["errors"]) {
-    const errorMessage = `
-    Before running the application, you need to set up your environment variables:
-    1. Copy the '.env.example' file in the root directory and rename the copy to '.env'.
-    2. Open the '.env' file and fill in the necessary values as described in the readme.md.`;
-    console.error(errorMessage);
-    console.error("Zod Errors: ", errors);
-}
-
-// log environment variable warning in console but don't block.
-export const env = envResult.success
-    ? envResult.data
-    : (() => {
-          logEnvErrors(envResult.error.errors);
-          return {} as typeof envSchema._type;
-      })();
