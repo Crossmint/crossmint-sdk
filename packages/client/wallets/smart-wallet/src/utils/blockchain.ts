41c5fb2ebae4957ec4ea3262868eba4f0c14e861
Devlyn Dorfer <devlyndorfer@gmail.com>
2024-07-30 09:35:40 -0400

    drop unused function to be re-added later if needed

--- packages/client/wallets/smart-wallet/src/utils/blockchain.ts
+++ packages/client/wallets/smart-wallet/src/utils/blockchain.ts
@@ -1,16 +1,4 @@
-import { EVMBlockchainIncludingTestnet } from "@crossmint/common-sdk-base";
-
 import { SmartWalletChain } from "..";
-
-function isPolygonCDK(chain: EVMBlockchainIncludingTestnet) {
-    const polygonCDKchains: EVMBlockchainIncludingTestnet[] = [
-        EVMBlockchainIncludingTestnet.ZKYOTO,
-        EVMBlockchainIncludingTestnet.ZKATANA,
-        EVMBlockchainIncludingTestnet.ASTAR_ZKEVM,
-        EVMBlockchainIncludingTestnet.HYPERSONIC_TESTNET,
-    ];
-    return polygonCDKchains.includes(chain);
-}
 
 export function usesGelatoBundler(chain: SmartWalletChain) {
     return false;
