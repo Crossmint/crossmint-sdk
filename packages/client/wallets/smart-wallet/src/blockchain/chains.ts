9021d7ec3aec369072ad7b1d2aad19f43b2a8a31
alberto-crossmint <alberto@paella.dev>
2024-08-02 16:16:09 +0200

    AA: Add Optimism mainnet and testnet

--- packages/client/wallets/smart-wallet/src/blockchain/chains.ts
+++ packages/client/wallets/smart-wallet/src/blockchain/chains.ts
@@ -1,11 +1,12 @@
 import { BUNDLER_RPC } from "@/utils/constants";
-import { Chain, base, baseSepolia, polygon, polygonAmoy } from "viem/chains";
+import { Chain, base, baseSepolia, polygon, polygonAmoy, optimism, optimismSepolia, } from "viem/chains";
 
 import { BlockchainIncludingTestnet as Blockchain, ObjectValues, objectValues } from "@crossmint/common-sdk-base";
 
 export const SmartWalletTestnet = {
     BASE_SEPOLIA: Blockchain.BASE_SEPOLIA,
     POLYGON_AMOY: Blockchain.POLYGON_AMOY,
+    OPTIMISM_SEPOLIA: Blockchain.OPTIMISM_SEPOLIA,
 } as const;
 export type SmartWalletTestnet = ObjectValues<typeof SmartWalletTestnet>;
 export const SMART_WALLET_TESTNETS = objectValues(SmartWalletTestnet);
@@ -13,6 +14,7 @@ export const SMART_WALLET_TESTNETS = objectValues(SmartWalletTestnet);
 export const SmartWalletMainnet = {
     BASE: Blockchain.BASE,
     POLYGON: Blockchain.POLYGON,
+    OPTIMISM: Blockchain.OPTIMISM,
 } as const;
 export type SmartWalletMainnet = ObjectValues<typeof SmartWalletMainnet>;
 export const SMART_WALLET_MAINNETS = objectValues(SmartWalletMainnet);
@@ -29,6 +31,8 @@ export const zerodevProjects: Record<SmartWalletChain, string> = {
     "polygon-amoy": "3deef404-ca06-4a5d-9a58-907c99e7ef00",
     "base-sepolia": "5a127978-6473-4784-9dfb-f74395b220a6",
     base: "e8b3020f-4dde-4176-8a7d-be8102527a5c",
+    "optimism-sepolia": "f8dd488e-eaed-467d-a5de-0184c160f3b1",
+    optimism: "505950ab-ee07-4a9c-bd16-320ac71a9350",
 };
 
 export const viemNetworks: Record<SmartWalletChain, Chain> = {
@@ -36,6 +40,8 @@ export const viemNetworks: Record<SmartWalletChain, Chain> = {
     "polygon-amoy": polygonAmoy,
     base: base,
     "base-sepolia": baseSepolia,
+    optimism: optimism,
+    "optimism-sepolia": optimismSepolia,
 };
 
 export const getBundlerRPC = (chain: SmartWalletChain) => {
