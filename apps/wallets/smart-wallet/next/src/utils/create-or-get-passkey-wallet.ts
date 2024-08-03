ca9a90847afe1128c5b5ebddd9fa8a05c72e396e
jmderby <jonathan.temp@paella.dev>
2024-08-02 20:09:19 -0700

    removed zod

--- apps/wallets/smart-wallet/next/src/utils/create-or-get-passkey-wallet.ts
+++ apps/wallets/smart-wallet/next/src/utils/create-or-get-passkey-wallet.ts
@@ -1,10 +1,8 @@
-import { env } from "@/env";
-
 import { Chain, SmartWalletSDK } from "@crossmint/client-sdk-smart-wallet";
 
 export async function createOrGetPasskeyWallet(jwt: string) {
     const xm = SmartWalletSDK.init({
-        clientApiKey: env.NEXT_PUBLIC_CROSSMINT_API_KEY,
+        clientApiKey: process.env.NEXT_PUBLIC_CROSSMINT_API_KEY!,
     });
 
     const test = await xm.getOrCreateWallet({ jwt }, Chain.POLYGON_AMOY);
