395aa09ab53314d4c01eaa730c59fafe398c0f83
jmderby <jonathan.temp@paella.dev>
2024-08-02 17:36:52 -0700

    fixed build

--- apps/wallets/smart-wallet/next/src/utils/create-or-get-passkey-wallet.ts
+++ apps/wallets/smart-wallet/next/src/utils/create-or-get-passkey-wallet.ts
@@ -1,13 +1,13 @@
 import { env } from "@/env";
 
-import { SmartWalletChain, SmartWalletSDK } from "@crossmint/client-sdk-smart-wallet";
+import { Chain, SmartWalletSDK } from "@crossmint/client-sdk-smart-wallet";
 
 export async function createOrGetPasskeyWallet(jwt: string) {
     const xm = SmartWalletSDK.init({
         clientApiKey: env.NEXT_PUBLIC_CROSSMINT_API_KEY_STG || "",
     });
 
-    const test = await xm.getOrCreateWallet({ jwt }, SmartWalletChain.POLYGON_AMOY);
+    const test = await xm.getOrCreateWallet({ jwt }, Chain.POLYGON_AMOY);
 
     return test;
 }
