355965e52f608839ac326f37e24daddbc2674125
Guido Contreras Woda <guiwoda@gmail.com>
2024-08-06 16:32:00 -0300

    Remove unused code

--- apps/wallets/smart-wallet/react/src/app/auth/PrivyAuthManager.ts
+++ apps/wallets/smart-wallet/react/src/app/auth/PrivyAuthManager.ts
@@ -1,16 +1,5 @@
 import { PrivyInterface } from "@privy-io/react-auth";
 
-export const parseToken = (token: any) => {
-    try {
-        const base64Url = token.split(".")[1];
-        const base64 = base64Url.replace("-", "+").replace("_", "/");
-        return JSON.parse(window.atob(base64 || ""));
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
-};
-
 export const signInWithPrivy = async (privy: PrivyInterface): Promise<string | null> => {
     privy.login();
     return privy.getAccessToken();
