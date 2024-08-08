511e3f0fef8bdfea3596b10a6c7a413b9e57d4b1
vmc-crossmint <valerio@paella.dev>
2024-08-08 22:21:45 +0200

    Better types docs

--- packages/client/verifiable-credentials/src/verifiableCredentialsSDK/types/collection.ts
+++ packages/client/verifiable-credentials/src/verifiableCredentialsSDK/types/collection.ts
@@ -1,47 +1,92 @@
 import { VCChain } from "./chain";
 import { Nft } from "./nft";
 
+/**
+ * Collection represents a generic collection of NFTs.
+ */
 export interface Collection {
+    /**
+     * List of NFTs within the collection.
+     */
     nfts: Nft[];
+
+    /**
+     * Address of the contract associated with the collection.
+     */
     contractAddress: string;
+
+    /**
+     * The blockchain chain on which the contract is deployed.
+     */
     chain: VCChain;
+
+    /**
+     * Metadata associated with the collection.
+     */
     metadata: any;
 }
 
 // VC Contract types
 
 /**
- * CredentialsCollection is a collection of credentials nfts
- * @param nfts - List of nfts
- * @param contractAddress - Address of the contract associated with the collection
- * @param chain - Chain on which the contract is deployed
- * @param metadata - Metadata of the contract
- * @param metadata.credentialMetadata - Credentials specific metadata
+ * CredentialsCollection represents a collection of credential NFTs.
+ *
+ * @extends Collection
  */
 export interface CredentialsCollection extends Collection {
+    /**
+     * Metadata of the contract, including credential-specific metadata.
+     */
     metadata: VCContractMetadata;
 }
 
+/**
+ * VCContractMetadata contains metadata related to the contract, including credentials specific metadata.
+ */
 export interface VCContractMetadata {
+    /**
+     * Credential-specific metadata inside the contract metadata.
+     */
     credentialMetadata: CredentialMetadata;
 }
 
 /**
- * CredentialMetadata is credentials specific metadata inside the contract metadata
- * @param type - Specifies the type of the credentials in the collection
- * @param encryption - Specifies the encryption type of the credentials in the collection
- * @param credentialsEndpoint - Endpoint to fetch the credentials
- * @param issuerDid - DID of the issuer of the credentials for this collection
+ * CredentialMetadata represents credentials-specific metadata inside the contract metadata.
  */
 export interface CredentialMetadata {
+    /**
+     * Specifies the types of credentials in the collection.
+     */
     type: string[];
+
+    /**
+     * Specifies the encryption type used for the credentials in the collection.
+     */
     encryption: VerifiableCredentialEncryption;
+
+    /**
+     * Endpoint to fetch the credentials.
+     */
     credentialsEndpoint: string;
+
+    /**
+     * The DID (Decentralized Identifier) of the issuer of the credentials for this collection.
+     */
     issuerDid: string;
 }
 
+/**
+ * VerifiableCredentialEncryption defines the encryption settings for verifiable credentials collection.
+ */
 export interface VerifiableCredentialEncryption {
+    /**
+     * Specifies the type of encryption used for the verifiable credentials.
+     */
     type: VerifiableCredentialEncryptionType;
+
+    /**
+     * Optional additional details about the encryption useful for decryption.
+     */
     details?: LitEncryptionDetails;
 }
 
@@ -49,8 +94,22 @@ export interface LitEncryptionDetails {
     network: string;
 }
 
+/**
+ * VerifiableCredentialEncryptionType defines the types of encryption used for the credentials.
+ */
 export enum VerifiableCredentialEncryptionType {
+    /**
+     * No encryption applied to the verifiable credentials.
+     */
     NONE = "none",
+
+    /**
+     * Decentralized encryption using the Lit protocol.
+     */
     DECENTRALIZED_LIT = "decentralized-lit",
+
+    /**
+     * Wallet based encryption that is recoverable via Crossmint.
+     */
     CROSSMINT_RECOVERABLE = "crossmint-recoverable",
 }
--- packages/client/verifiable-credentials/src/verifiableCredentialsSDK/types/credentialFilter.ts
+++ packages/client/verifiable-credentials/src/verifiableCredentialsSDK/types/credentialFilter.ts
@@ -1,9 +1,16 @@
 /**
- *  Credential filter
- *  @param issuers - List of issuers to filter by eg ["did:polygon-amoy:0x1B887669437644aA348c518844660ef8d63bd643"]
- *  @param types - List of types accepted eg ["driving_license", "crossmint:e62564a7-06eb-4f65-b389-eb3b7a4f6f98:userAge"]
+ * CredentialFilter defines the criteria for filtering credentials.
  */
 export interface CredentialFilter {
+    /**
+     * List of issuers to filter by.
+     * @example ["did:polygon-amoy:0x1B887669437644aA348c518844660ef8d63bd643"]
+     */
     issuers?: string[];
+
+    /**
+     * List of accepted credential types.
+     * @example ["driving_license", "crossmint:e62564a7-06eb-4f65-b389-eb3b7a4f6f98:userAge"]
+     */
     types?: string[];
 }
