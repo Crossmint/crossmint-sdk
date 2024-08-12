c1e165b1c436fb03dc884be945faa9fe36bf3e87
Valerio Massimo <140358313+vmc-crossmint@users.noreply.github.com>
2024-08-12 11:46:07 -0400

    VC: docs  (#629)
    * Docs first draft
    
    * refiend comments and included additional ones
    
    * wip add typedoc created docs
    
    * fix formatting
    
    * Better types docs
    
    * small changes in docs for sorting params
    
    * fix issues
    
    * prep for typedoc generation
    
    * remove no longer needed python scripts
    
    * updated custom plugin and typedoc.json testing filtering
    
    * wip on custom grouping
    
    * small edit for visual distinction of credentialService operations
    
    * final updates for running typedoc on vc sdk
    
    
    ---------
    
    Co-authored-by: Filippos Lymperopoulos <filippos@paella.dev>
    Co-authored-by: Danny Mulvihill <danny@paella.dev>

---
+++ packages/client/verifiable-credentials/custom-plugin.mjs
@@ -0,0 +1,30 @@
+import pkg from 'typedoc-plugin-markdown';
+const { MarkdownPageEvent } = pkg;
+ 
+export function load(app) {
+  app.renderer.postRenderAsyncJobs.push(async (renderer) => {
+    const navigation = renderer.navigation;
+
+    const resultArray = navigation.map(item => ({
+        group: item.title,
+        pages: item.children.map(child => `sdk-reference/credentials/${child.path.replace('.mdx', '')}`)
+    }));
+
+    // simply output to console, we then manually copy/paste into mint.json in Mintlify
+    console.log(JSON.stringify(resultArray));
+  });
+
+  // this adds the title to top of every mdx file as Mintlify expects
+  // @todo - add description to important SDK classes and set `description` here also
+  app.renderer.on(MarkdownPageEvent.BEGIN, (page) => {
+    page.frontmatter = {
+      title: page.model?.name,
+      ...page.frontmatter,
+    };
+  });
+
+  app.renderer.on(MarkdownPageEvent.END, (page) => {
+    // remove .mdx from links so it works with mintlify
+    page.contents = page.contents.replace(/\.mdx/g, "");
+  });
+}
--- packages/client/verifiable-credentials/src/decryption/wallet.ts
+++ packages/client/verifiable-credentials/src/decryption/wallet.ts
@@ -6,8 +6,21 @@ import {
     isVerifiableCredential,
 } from "@/verifiableCredentialsSDK";
 
+/**
+ * Class for decrypting verifiable credentials that have been encrypted with the `VerifiableCredentialEncryptionType.CROSSMINT_RECOVERABLE` encryption type.
+ *
+ * This class uses a provided signature callback to authenticate the user and decrypt the credential.
+ * To use the Crossmint decrypt endpoint, an API key with the `credentials.decrypt` scope must be provided.
+ */
 export class CrossmintDecrypt {
     userAddress: string;
+
+    /**
+     * A callback function that signs the challenge with the user's wallet.
+     * @param wallet - The user's wallet address.
+     * @param challenge - The challenge string that needs to be signed.
+     * @returns A promise that resolves to the signature string.
+     */
     signCallback: (wallet: string, challenge: string) => Promise<string>;
     constructor(
         userAddress: string,
@@ -17,6 +30,7 @@ export class CrossmintDecrypt {
         this.userAddress = userAddress;
         this.signCallback = signCallback;
     }
+
     private async tryDecrypt(credential: EncryptedVerifiableCredential): Promise<any> {
         const challenge = await this.authService.getChallenge(this.userAddress);
         console.log("Waiting for signature from user");
@@ -25,6 +39,16 @@ export class CrossmintDecrypt {
         return this.authService.decrypt(credential, challenge, signature, this.userAddress);
     }
 
+    /**
+     * Decrypts an encrypted verifiable credential.
+     *
+     * This method validates the decrypted data to ensure it is a valid verifiable credential.
+     *
+     * @param credential - The encrypted verifiable credential to decrypt.
+     * @returns A promise that resolves to a `VerifiableCredential`.
+     *
+     * @throws Will throw an error if the decrypted data is not a valid verifiable credential.
+     */
     async decrypt(credential: EncryptedVerifiableCredential): Promise<VerifiableCredential> {
         const vc = await this.tryDecrypt(credential);
         if (!isVerifiableCredential(vc)) {
@@ -34,11 +58,29 @@ export class CrossmintDecrypt {
     }
 }
 
+/**
+ * Class for decrypting verifiable credentials encrypted with the `VerifiableCredentialEncryptionType.CROSSMINT_RECOVERABLE` encryption type using Metamask.
+ *
+ * This class uses Metamask to prompt the user to sign a message to decrypt the credential.
+ * If you want to use a different signature method, refer to the `CrossmintDecrypt` class.
+ * To use the Crossmint decrypt endpoint, an API key with the `credentials.decrypt` scope must be provided.
+ */
 export class CrossmintMetamaskDecrypt {
     constructor(private readonly metamask = new MetamaskService()) {
         this.metamask.metamaskSignMessage = this.metamask.metamaskSignMessage.bind(this.metamask);
     }
 
+    /**
+     * Decrypts an encrypted verifiable credential using Metamask.
+     *
+     * This method prompts the user via Metamask to sign the necessary message for decrypting the credential.
+     *
+     * @param credential - The encrypted verifiable credential to decrypt.
+     * @param wallet - (Optional) The user's wallet address. If not provided, the connected wallet address from Metamask will be used.
+     * @returns A promise that resolves to a `VerifiableCredential`.
+     *
+     * @throws Will throw an error if decryption fails or if Metamask is not properly configured.
+     */
     async decrypt(credential: EncryptedVerifiableCredential, wallet?: string): Promise<VerifiableCredential> {
         wallet = wallet || (await this.metamask.getConnectedWallet());
         const descryptService = new CrossmintDecrypt(wallet, this.metamask.metamaskSignMessage);
