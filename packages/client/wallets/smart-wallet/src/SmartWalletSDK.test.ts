ff3806fb33d7c9779c3caab66394ed0c06720fc3
Devlyn Dorfer <devlyndorfer@gmail.com>
2024-08-13 16:47:39 -0400

    fix import within unit test

--- packages/client/wallets/smart-wallet/src/SmartWalletSDK.test.ts
+++ packages/client/wallets/smart-wallet/src/SmartWalletSDK.test.ts
@@ -1,5 +1,5 @@
 import { SmartWalletSDK } from "./SmartWalletSDK";
-import { SmartWalletSDKInitParams } from "./types/Config";
+import { SmartWalletSDKInitParams } from "./types/config";
 
 jest.mock("./services/logging");
 
