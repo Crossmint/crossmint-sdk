2b42ce649b06621b9546d514ab5e9898521b2499
jmderby <jonathan.temp@paella.dev>
2024-08-14 15:39:55 -0700

    testing to see if commit works

--- packages/client/wallets/smart-wallet/src/blockchain/chains.ts
+++ packages/client/wallets/smart-wallet/src/blockchain/chains.ts
@@ -40,7 +40,7 @@ export const SMART_WALLET_CHAINS = objectValues(SmartWalletChain);
 
 export const zerodevProjects: Record<SmartWalletChain, string> = {
     polygon: "5c9f4865-ca8e-44bb-9b9e-3810b2b46f9f",
-    "polygon-amoy": "96021f51-61b7-4385-b57f-e81f3f4c4f70",
+    "polygon-amoy": "3deef404-ca06-4a5d-9a58-907c99e7ef00",
     "base-sepolia": "5a127978-6473-4784-9dfb-f74395b220a6",
     base: "e8b3020f-4dde-4176-8a7d-be8102527a5c",
     "optimism-sepolia": "f8dd488e-eaed-467d-a5de-0184c160f3b1",
