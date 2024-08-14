f2a258987a706abdb4c0e29843eecdd7fb014963
jmderby <jonathan.temp@paella.dev>
2024-08-14 14:23:04 -0700

    fixed build error

---
+++ apps/payments/nextjs/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+    experimental: {
+        esmExternals: "loose",
+    },
+};
+
+module.exports = nextConfig;
--- packages/client/ui/react-ui/src/providers/CrossmintAuthProvider.tsx
+++ packages/client/ui/react-ui/src/providers/CrossmintAuthProvider.tsx
@@ -26,31 +26,31 @@ export function CrossmintAuthProvider(props: AuthWalletProviderParams) {
 function AuthWalletProvider(props: AuthWalletProviderParams) {
     const { jwt } = useAuthCore();
 
-    // const [wallet, setWallet] = useState<EVMSmartWallet | null>(null);
+    const [wallet, setWallet] = useState<EVMSmartWallet | null>(null);
 
-    // const smartWalletSDK = useMemo(() => SmartWalletSDK.init({ clientApiKey: props.apiKey }), undefined);
+    const smartWalletSDK = useMemo(() => SmartWalletSDK.init({ clientApiKey: props.apiKey }), [props.apiKey]);
 
-    // const createWallet = async (jwt: string) => {
-    //     try {
-    //         const wallet = await smartWalletSDK.getOrCreateWallet({ jwt }, props.embeddedWallets.defaultChain);
-    //         setWallet(wallet);
-    //     } catch (e: any) {
-    //         console.error("There was an error creating a wallet ", e);
-    //         throw e;
-    //     }
-    // };
+    const createWallet = async (jwt: string) => {
+        try {
+            const wallet = await smartWalletSDK.getOrCreateWallet({ jwt }, props.embeddedWallets.defaultChain);
+            setWallet(wallet);
+        } catch (e: any) {
+            console.error("There was an error creating a wallet ", e);
+            throw e;
+        }
+    };
 
     useEffect(() => {
         if (props.embeddedWallets.createOnLogin && jwt) {
             console.log("Creating wallet");
-            // createWallet(jwt);
+            createWallet(jwt);
         }
 
-        // if (wallet && !jwt) {
-        //     // implies a logout has occurred, clear wallet
-        //     console.log("Clearing wallet");
-        //     setWallet(null);
-        // }
+        if (wallet && !jwt) {
+            // implies a logout has occurred, clear wallet
+            console.log("Clearing wallet");
+            setWallet(null);
+        }
     }, [jwt, props.embeddedWallets.createOnLogin]);
 
     return <WalletContext.Provider value={{ wallet: null }}>{props.children}</WalletContext.Provider>;
