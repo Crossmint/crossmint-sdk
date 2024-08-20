b21c046336164160c94c8fcd343f022b2e546d5d
matinavdev <matias@paella.dev>
2024-08-20 12:54:52 -0300

    remove console logs

--- packages/client/auth-core/src/utils/tokenAuth/publicKey.ts
+++ packages/client/auth-core/src/utils/tokenAuth/publicKey.ts
@@ -4,17 +4,13 @@ import { JwksClient } from "jwks-rsa";
 import PublicKeyCacheProvider from "../PublicKeyCacheProvider";
 
 export async function getFetchedPublicKey(token: string, jwksUri: string) {
-    console.log("Getting fetched public key from jwksUri", jwksUri);
     const decoded = decode(token, { complete: true });
-    console.log("Decoded token", decoded);
     if (decoded?.header == null) {
         throw new Error("Invalid Token: Header Formatted Incorrectly");
     }
 
     const signingKey = await getSigningKey(jwksUri, decoded.header.kid);
-    console.log("Signing key", signingKey);
     const publicKey = signingKey.getPublicKey();
-    console.log("Setting cached public key in localStorage", publicKey);
     PublicKeyCacheProvider.setCachedPublicKey(publicKey);
 
     return publicKey;
@@ -23,11 +19,9 @@ export async function getFetchedPublicKey(token: string, jwksUri: string) {
 export async function getCachedPublicKey(token: string, jwksUri: string) {
     const publicKey = PublicKeyCacheProvider.getCachedPublicKey();
     if (publicKey != null) {
-        console.log("Cached public key found, returning cached public key");
         return publicKey;
     }
 
-    console.log("No cached public key found, fetching from jwksUri");
     return getFetchedPublicKey(token, jwksUri);
 }
 
@@ -40,11 +34,8 @@ async function getSigningKey(jwksUri: string, kid?: string) {
     console.log("Getting signing key from jwksUri", jwksUri);
     try {
         const signingKey = await client.getSigningKey(kid);
-        console.log("Signing key found", signingKey);
         return signingKey;
     } catch (error) {
-        console.log("Signing key not found, throwing error");
-        console.error(error, "some error");
         throw new Error("Unable to retrieve signing key");
     }
 }
