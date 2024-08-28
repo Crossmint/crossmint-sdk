5b8fb8755fc09a9c438a94adaed93234dad1b855
jmderby <jonathan.temp@paella.dev>
2024-08-28 19:07:45 +0200

    removed appearance prop

--- packages/client/ui/react-ui/src/providers/CrossmintAuthProvider.tsx
+++ packages/client/ui/react-ui/src/providers/CrossmintAuthProvider.tsx
@@ -1,21 +1,20 @@
 import { useCrossmint } from "@/hooks";
 import { ReactNode } from "react";
 
-import { AuthProvider as AuthCoreProvider, type AuthModalAppearance } from "@crossmint/client-sdk-auth-core/client";
+import { AuthProvider as AuthCoreProvider } from "@crossmint/client-sdk-auth-core/client";
 
 import { CrossmintWalletConfig, CrossmintWalletProvider } from "./CrossmintWalletProvider";
 
 type CrossmintAuthProviderProps = {
     embeddedWallets: CrossmintWalletConfig;
     children: ReactNode;
-    appearance?: AuthModalAppearance;
 };
 
-export function CrossmintAuthProvider({ embeddedWallets, children, appearance }: CrossmintAuthProviderProps) {
+export function CrossmintAuthProvider({ embeddedWallets, children }: CrossmintAuthProviderProps) {
     const { crossmint, setJwt } = useCrossmint("CrossmintAuthProvider must be used within CrossmintProvider");
 
     return (
-        <AuthCoreProvider setJwtToken={setJwt} crossmint={crossmint} appearance={appearance}>
+        <AuthCoreProvider setJwtToken={setJwt} crossmint={crossmint}>
             <CrossmintWalletProvider config={embeddedWallets}>{children}</CrossmintWalletProvider>
         </AuthCoreProvider>
     );
