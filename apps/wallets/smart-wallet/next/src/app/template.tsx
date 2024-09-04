5624fc11a0e152bd1bee82119dd94dfc81624381
jmderby <jonathan.temp@paella.dev>
2024-09-04 18:55:17 +0200

    updated template file

--- apps/wallets/smart-wallet/next/src/app/template.tsx
+++ apps/wallets/smart-wallet/next/src/app/template.tsx
@@ -2,13 +2,21 @@
 
 import { Header } from "@/components/header";
 import { Toaster } from "@/components/toaster";
+import { ReactNode } from "react";
 
-export default function Template({ children }: { children: React.ReactNode }) {
+import { Providers } from "./_lib/providers";
+
+// Using a template file is a workaround for the rendering issue:
+// Templates create new instances for each child on navigation, unlike layouts.
+// This forces a re-render of components, potentially avoiding the development server hang.
+// The issue occurs when combining Server Components, Client Providers, and global CSS.
+// For more details, see the issue on Next.js Github: https://github.com/vercel/next.js/issues/69682
+export default function Template({ children }: { children: ReactNode }) {
     return (
-        <>
+        <Providers>
             <Header />
             <Toaster />
             {children}
-        </>
+        </Providers>
     );
 }
