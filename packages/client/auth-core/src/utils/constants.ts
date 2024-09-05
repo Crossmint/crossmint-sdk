0c5aab0527c932d503c2594fef9c4419f4140c9c
jmderby <jonathan.temp@paella.dev>
2024-09-05 19:50:49 +0200

    removed unused code

--- packages/client/auth-core/src/utils/constants.ts
+++ packages/client/auth-core/src/utils/constants.ts
@@ -1,10 +1,1 @@
-export const CROSSMINT_STG_URL = "https://staging.crossmint.com";
-export const CROSSMINT_PROD_URL = "https://www.crossmint.com";
-
-export const CrossmintEnvironment = {
-    STAGING: "staging",
-    PRODUCTION: "production",
-} as const;
-export type CrossmintEnvironment = (typeof CrossmintEnvironment)[keyof typeof CrossmintEnvironment];
-
 export const AUTH_SERVICE = "AUTH_SDK";
