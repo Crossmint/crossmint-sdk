77d184622ead9c49ccd49dc7bfcd21a87f56852a
Devlyn <devlyndorfer@gmail.com>
2024-09-10 20:07:48 +0100

    Smart Wallets: Export `executeContract` Errors (#743)
    * export transaction errors
    
    * add changeset
    
    * export smart wallet errors from react ui

---
+++ .changeset/happy-swans-sell.md
@@ -0,0 +1,5 @@
+---
+"@crossmint/client-sdk-react-ui": patch
+---
+
+Export smart wallet errors
--- packages/client/ui/react-ui/src/index.ts
+++ packages/client/ui/react-ui/src/index.ts
@@ -3,5 +3,20 @@ export * from "./hooks";
 export * from "./providers";
 
 export { CrossmintEvents, useCrossmintEvents } from "@crossmint/client-sdk-base";
-export { type EVMSmartWallet, Chain } from "@crossmint/client-sdk-smart-wallet";
+export {
+    type EVMSmartWallet,
+    Chain,
+    SmartWalletError,
+    UserWalletAlreadyCreatedError,
+    AdminAlreadyUsedError,
+    AdminMismatchError,
+    PasskeyMismatchError,
+    PasskeyPromptError,
+    PasskeyRegistrationError,
+    PasskeyIncompatibleAuthenticatorError,
+    ConfigError,
+    SmartWalletsNotEnabledError,
+    EVMSendTransactionError,
+    EVMSendTransactionExecutionRevertedError,
+} from "@crossmint/client-sdk-smart-wallet";
 export type { CrossmintEvent, CrossmintEventMap } from "@crossmint/client-sdk-base";
