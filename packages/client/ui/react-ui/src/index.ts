265fb88d5ed715a98d537311f45a41a3c8db2aca
Alberto Elias <hi@albertoelias.me>
2024-09-24 23:27:28 +0200

    @crossmint/client-sdk-react-ui: Twind (Tailwind-in-js) (#764)
    * @crossmint/client-sdk-react-ui: css-in-js (Tailwind)
    
    * used relative import
    
    * added changeset
    
    * renamed fn and folder

--- packages/client/ui/react-ui/src/index.ts
+++ packages/client/ui/react-ui/src/index.ts
@@ -1,3 +1,11 @@
+import { install } from "@twind/core";
+
+import twindConfig from "./twind.config";
+
+// Initialize twind with custom configuration
+// This sets up the CSS-in-JS styling solution for the entire application
+install(twindConfig);
+
 export * from "./components";
 export * from "./hooks";
 export * from "./providers";
