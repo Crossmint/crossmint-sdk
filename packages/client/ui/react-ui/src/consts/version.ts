7b81998485cffcada0e2cdb9914e724765d0526c
Alberto Elias <hi@albertoelias.me>
2024-09-24 23:00:56 +0200

    @crossmint/client-sdk-react-ui: Twind (Tailwind-in-js) (#764)
    * @crossmint/client-sdk-react-ui: css-in-js (Tailwind)
    
    * used relative import
    
    * added changeset
    
    * renamed fn and folder

--- packages/client/ui/react-ui/src/consts/version.ts
+++ packages/client/ui/react-ui/src/consts/version.ts
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 import { version } from "../../package.json";
 
 export const LIB_VERSION = version;
+=======
+export const LIB_VERSION = "1.3.21";
+>>>>>>> 5a941b840a6c (@crossmint/client-sdk-react-ui: Twind (Tailwind-in-js) (#764))
