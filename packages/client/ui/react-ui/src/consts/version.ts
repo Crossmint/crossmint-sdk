56427dc4699b3b58b6cbae51dd6082ea511ff741
Alberto Elias <hi@albertoelias.me>
2024-09-24 23:14:47 +0200

    fixes more merge conflicts

--- packages/client/ui/react-ui/src/consts/version.ts
+++ packages/client/ui/react-ui/src/consts/version.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import { version } from "../../package.json";
 
 export const LIB_VERSION = version;
-=======
-export const LIB_VERSION = "1.3.21";
->>>>>>> 5a941b840a6c (@crossmint/client-sdk-react-ui: Twind (Tailwind-in-js) (#764))
