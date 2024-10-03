31d9cd18cd18d2c332c411cca4ed8ee500ad38fe
matinavdev <matias@paella.dev>
2024-10-03 16:38:37 -0300

    remove

--- apps/wallets/smart-wallet/next/src/components/mint-nft-button.tsx
+++ apps/wallets/smart-wallet/next/src/components/mint-nft-button.tsx
@@ -5,7 +5,7 @@ import { Spinner } from "@/icons/spinner";
 import { mintNFT } from "@/utils/mint-api";
 import { useState } from "react";
 
-import { useAuth, useWallet } from "@crossmint/client-sdk-react-ui";
+import { useWallet } from "@crossmint/client-sdk-react-ui";
 
 import { Button } from "./button";
 import { Typography } from "./typography";
@@ -14,7 +14,6 @@ import { useToast } from "./use-toast";
 export const MintNFTButton = ({ setNftSuccessfullyMinted }: { setNftSuccessfullyMinted: (a: boolean) => void }) => {
     const { wallet } = useWallet();
     const [isLoadingMint, setIsLoadingMint] = useState(false);
-    const { user, getUser } = useAuth();
     const { toast } = useToast();
 
     if (isLoadingMint) {
