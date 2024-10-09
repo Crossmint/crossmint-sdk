8460b0ee33856e6c35000f9fe1c3636034d99c4c
Alberto Elias <hi@albertoelias.me>
2024-10-09 15:54:25 +0200

    Support building ssr app in CI

--- apps/auth/ssr/src/app/page.tsx
+++ apps/auth/ssr/src/app/page.tsx
@@ -1,13 +1,17 @@
 import { createCrossmint, CrossmintAuth } from "@crossmint/server-sdk";
 
 export default function Home() {
-    const crossmint = createCrossmint({
-        apiKey: process.env.SERVER_CROSSMINT_API_KEY || "",
-    });
-    const crossmintAuth = CrossmintAuth.from(crossmint);
+    try {
+        const crossmint = createCrossmint({
+            apiKey: process.env.SERVER_CROSSMINT_API_KEY || "",
+        });
+        const crossmintAuth = CrossmintAuth.from(crossmint);
 
-    console.log(crossmint);
-    console.log(crossmintAuth);
+        console.log(crossmint);
+        console.log(crossmintAuth);
+    } catch (error) {
+        console.error(error);
+    }
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
