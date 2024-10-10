f599d1665e61e5c3dfb1cdd2cd6009c8df6613a5
matinavdev <matias@paella.dev>
2024-10-10 12:35:37 -0300

    add getUser on the server

--- packages/server/src/auth/CrossmintAuth.ts
+++ packages/server/src/auth/CrossmintAuth.ts
@@ -1,6 +1,8 @@
 import { type Crossmint, CrossmintApiClient } from "@crossmint/common-sdk-base";
 import { SDK_NAME, SDK_VERSION } from "./utils/constants";
 
+const CROSSMINT_API_VERSION = "2024-09-26";
+
 export class CrossmintAuth {
     private crossmint: Crossmint;
     private apiClient: CrossmintApiClient;
@@ -19,5 +21,16 @@ export class CrossmintAuth {
 
     public static from(crossmint: Crossmint): CrossmintAuth {
         return new CrossmintAuth(crossmint);
+    }
+
+    async getUser(externalUserId: string) {
+        const result = await this.apiClient.get(`api/${CROSSMINT_API_VERSION}/sdk/auth/user/${externalUserId}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const user = await result.json();
+        return user;
     }
 }
