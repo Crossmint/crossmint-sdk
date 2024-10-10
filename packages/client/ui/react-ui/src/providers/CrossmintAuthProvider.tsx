fef32529597f8abf11eab86881532ff035f026ce
Jonathan <jonathan.temp@paella.dev>
2024-10-10 08:25:38 -0700

    Auth modal: Login Options + Responsive height (#794)
    * Auth Modal: Login options
    
    * lint fix
    
    * added changeset
    
    * added height changes in this pr as i need them
    
    * added conditional for farcaster div height
    
    * review feedback

--- packages/client/ui/react-ui/src/providers/CrossmintAuthProvider.tsx
+++ packages/client/ui/react-ui/src/providers/CrossmintAuthProvider.tsx
@@ -18,10 +18,13 @@ export type CrossmintAuthWalletConfig = {
     showWalletModals?: boolean;
 };
 
+export type LoginMethod = "email" | "google" | "farcaster";
+
 export type CrossmintAuthProviderProps = {
-    embeddedWallets: CrossmintAuthWalletConfig;
+    embeddedWallets?: CrossmintAuthWalletConfig;
     appearance?: UIConfig;
     children: ReactNode;
+    loginMethods?: LoginMethod[];
 };
 
 type AuthStatus = "logged-in" | "logged-out" | "in-progress";
@@ -43,7 +46,18 @@ export const AuthContext = createContext<AuthContextType>({
     getUser: () => {},
 });
 
-export function CrossmintAuthProvider({ embeddedWallets, children, appearance }: CrossmintAuthProviderProps) {
+const defaultEmbeddedWallets: CrossmintAuthWalletConfig = {
+    defaultChain: "base-sepolia",
+    createOnLogin: "off",
+    type: "evm-smart-wallet",
+};
+
+export function CrossmintAuthProvider({
+    embeddedWallets = defaultEmbeddedWallets,
+    children,
+    appearance,
+    loginMethods = ["email", "google"],
+}: CrossmintAuthProviderProps) {
     const [user, setUser] = useState<SDKExternalUser | undefined>(undefined);
     const { crossmint, setJwt, setRefreshToken } = useCrossmint(
         "CrossmintAuthProvider must be used within CrossmintProvider"
@@ -148,6 +162,7 @@ export function CrossmintAuthProvider({ embeddedWallets, children, appearance }:
                               fetchAuthMaterial={fetchAuthMaterial}
                               apiKey={crossmint.apiKey}
                               appearance={appearance}
+                              loginMethods={loginMethods}
                           />,
 
                           document.body
