6a219b57d13b0c211aea00730450d0bf5decd2ae
Alberto Elias <hi@albertoelias.me>
2024-10-15 11:38:27 +0200

    Adds entrypoint to react-ui to force loading twind

--- packages/client/ui/react-ui/tsup.config.ts
+++ packages/client/ui/react-ui/tsup.config.ts
@@ -5,6 +5,7 @@ import { treeShakableConfig } from "../../../../tsup.config.base";
 const config: Options = {
     ...treeShakableConfig,
     external: ["react", "react-dom"],
+    entry: ["src/index.ts"],
 };
 
 export default config;
