c42899cd7013f982b46abcf1360c64d19ba8ae48
Alberto Elias <hi@albertoelias.me>
2024-10-17 16:13:52 +0200

    Stores new auth material cookies

--- packages/server/src/auth/types/request.ts
+++ packages/server/src/auth/types/request.ts
@@ -1,6 +1,7 @@
-import type { IncomingMessage } from "http";
+import type { IncomingMessage, ServerResponse } from "http";
 
 export type GenericRequest = IncomingMessage | Request;
+export type GenericResponse = ServerResponse | Response;
 
 export function isNodeRequest(request: GenericRequest): request is IncomingMessage {
     return "httpVersion" in request;
@@ -8,4 +9,12 @@ export function isNodeRequest(request: GenericRequest): request is IncomingMessa
 
 export function isFetchRequest(request: GenericRequest): request is Request {
     return "headers" in request && typeof request.headers.get === "function";
+}
+
+export function isNodeResponse(response: GenericResponse): response is ServerResponse {
+    return "setHeader" in response;
+}
+
+export function isFetchResponse(response: GenericResponse): response is Response {
+    return "headers" in response && typeof response.headers.append === "function";
 }
