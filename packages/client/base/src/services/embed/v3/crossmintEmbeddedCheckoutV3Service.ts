39969cce1f5385d290a500e26ddbc6ec82692ac4
Pablo Egido <peg@paella.dev>
2024-10-18 09:23:44 -0400

    embedded-v3: send sdk info in iframe (#819)
    * embedded-v3: send sdk info in iframe
    
    * refactor
    
    * fix
    
    * lint

--- packages/client/base/src/services/embed/v3/crossmintEmbeddedCheckoutV3Service.ts
+++ packages/client/base/src/services/embed/v3/crossmintEmbeddedCheckoutV3Service.ts
@@ -4,10 +4,13 @@ import { embeddedCheckoutV3IncomingEvents, embeddedCheckoutV3OutgoingEvents } fr
 import { IFrameWindow } from "@crossmint/client-sdk-window";
 import type { CrossmintApiClient } from "@crossmint/common-sdk-base";
 
-export type CrossmintEmbeddedCheckoutV3ServiceProps = { apiClient: CrossmintApiClient };
+export type CrossmintEmbeddedCheckoutV3ServiceProps = {
+    apiClient: CrossmintApiClient;
+};
 
 export function crossmintEmbeddedCheckoutV3Service({ apiClient }: CrossmintEmbeddedCheckoutV3ServiceProps) {
     function getIFrameUrl(props: CrossmintEmbeddedCheckoutV3Props) {
+        const sdkMetadata = apiClient["internalConfig"].sdkMetadata;
         const urlWithPath = apiClient.buildUrl("/sdk/2024-03-05/embedded-checkout");
         const queryParams = new URLSearchParams();
 
@@ -34,6 +37,7 @@ export function crossmintEmbeddedCheckoutV3Service({ apiClient }: CrossmintEmbed
         }
 
         queryParams.append("apiKey", apiClient.crossmint.apiKey);
+        queryParams.append("sdkMetadata", JSON.stringify(sdkMetadata));
 
         return `${urlWithPath}?${queryParams.toString()}`;
     }
