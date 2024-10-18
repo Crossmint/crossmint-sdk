57227baa8330b435c7eb4d8f945967b7f2695a51
Pablo Egido <peg@paella.dev>
2024-10-18 12:42:45 -0400

    v3: Rename component for v0 release (#820)
    * v3: rename component to alpha for now
    
    * duh

--- packages/client/ui/react-ui/src/components/embed/v3/CrossmintEmbeddedCheckoutV3.tsx
+++ packages/client/ui/react-ui/src/components/embed/v3/CrossmintEmbeddedCheckoutV3.tsx
@@ -2,6 +2,6 @@ import type { CrossmintEmbeddedCheckoutV3Props } from "@crossmint/client-sdk-bas
 
 import { EmbeddedCheckoutV3IFrame } from "./EmbeddedCheckoutV3IFrame";
 
-export function CrossmintEmbeddedCheckout(props: CrossmintEmbeddedCheckoutV3Props) {
+export function CrossmintEmbeddedCheckout_Alpha(props: CrossmintEmbeddedCheckoutV3Props) {
     return <EmbeddedCheckoutV3IFrame {...props} />;
 }
