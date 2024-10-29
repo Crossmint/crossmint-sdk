80d2420a6f12b540ffbbccb3686b29ff5763a3f8
Maxwell Fortney <maxwell@paella.dev>
2024-10-29 12:08:19 -0400

    save

--- apps/payments/nextjs/components/hosted-v3/HostedCheckoutV3ClientProviders.tsx
+++ apps/payments/nextjs/components/hosted-v3/HostedCheckoutV3ClientProviders.tsx
@@ -5,7 +5,7 @@ export function HostedCheckoutV3ClientProviders({ children }: { children: ReactN
     return (
         <CrossmintProvider
             overrideBaseUrl="https://dserver.maxf.io"
-            apiKey="ck_development_5Nx4yZXetY5QVxJUp8kg1vaUoRvPtWUiFKQPiMqDmquohnkTJyXc1ynAw6XA6NiiGekhwtTMebDc9wbcFVS5ePxDqLFa9qkiXHuRH8n2igPAUQ9xXsyQBizWvRVdTx2Koy9s4qm8kaAiSQj4CtwiyY8EEUeAqWcsVVLAhjYCSDpViCMfqRjfM4FcnqgSieoRqaE7A4sLJVQWybnYbWJA8cXE"
+            apiKey="sk_development_5ZUNkuhjP8aYZEgUTDfWToqFpo5zakEqte1db4pHZgPAVKZ9JuSvnKeGiqY654DoBuuZEzYz4Eb8gRV2ePqQ1fxTjEP8tTaUQdzbGfyG9RgyeN5YbqViXinqxk8EayEkAGtvSSgjpjEr6iaBptJtUFwPW59DjQzTQP6P8uZdiajenVg7bARGKjzFyByNuVEoz41DpRB4hDZNFdwCTuf5joFv"
         >
             <CrossmintCheckoutProvider>{children}</CrossmintCheckoutProvider>
         </CrossmintProvider>
