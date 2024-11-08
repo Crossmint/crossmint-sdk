2d16b1e2457ca000b08af1c0e48778175356cc4d
Maxwell Fortney <maxwell@paella.dev>
2024-11-08 18:22:55 -0500

    exports

--- packages/client/base/src/types/index.ts
+++ packages/client/base/src/types/index.ts
@@ -3,4 +3,9 @@ export * from "./events";
 export * from "./hosted";
 export * from "./embed";
 export * from "./system";
+
 export * from "./CryptoCurrency";
+export * from "./Locale";
+export * from "./PaymentMethod";
+export * from "./Recipient";
+export * from "./clientNames";
