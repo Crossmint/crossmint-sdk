98deadceeb167c8115e11145d4e8ebd13e6dafd5
Maxwell Fortney <maxwell@paella.dev>
2024-11-08 10:33:27 -0500

    save

--- packages/client/base/src/utils/i18n/dictionaries/deDE.ts
+++ packages/client/base/src/utils/i18n/dictionaries/deDE.ts
@@ -6,5 +6,15 @@ const deDE = {
         BUY_WITH_SOL: "Mit SOL kaufen",
         BUY_WITH_CREDIT_CARD: "Mit Kreditkarte kaufen",
     },
+    hostedCheckoutV3: {
+        paymentVariant: {
+            pay: "Bezahlen mit {0}",
+            buy: "Kaufen mit {0}",
+            subscribe: "Abonnieren mit {0}",
+        },
+        crossmint: "Crossmint",
+        crypto: "Krypto",
+        card: "Karte",
+    },
 };
 export default deDE;
--- packages/client/base/src/utils/i18n/dictionaries/frFR.ts
+++ packages/client/base/src/utils/i18n/dictionaries/frFR.ts
@@ -6,5 +6,15 @@ const frFR = {
         BUY_WITH_SOL: "Acheter avec SOL",
         BUY_WITH_CREDIT_CARD: "Acheter avec une carte de crédit",
     },
+    hostedCheckoutV3: {
+        paymentVariant: {
+            pay: "Payer avec {0}",
+            buy: "Acheter avec {0}",
+            subscribe: "S'abonner avec {0}",
+        },
+        crossmint: "Crossmint",
+        crypto: "Crypto",
+        card: "Carte",
+    },
 };
 export default frFR;
