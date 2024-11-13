7dfad48472f285c3d768b5db67e7ebd263a3399b
mPaella <93682696+mPaella@users.noreply.github.com>
2024-11-13 14:34:18 -0500

    hosted v3: tweaks (#861)
    save

--- packages/client/base/src/types/hosted/v3/CrossmintHostedCheckoutV3Props.ts
+++ packages/client/base/src/types/hosted/v3/CrossmintHostedCheckoutV3Props.ts
@@ -1,14 +1,33 @@
-import type { Locale } from "@/types";
-import type { EmbeddedCheckoutV3LineItem, EmbeddedCheckoutV3Payment, EmbeddedCheckoutV3Recipient } from "@/types/embed";
+import type { Locale, Currency as FiatCurrency, CryptoCurrency } from "@/types";
+import type { EmbeddedCheckoutV3LineItem, EmbeddedCheckoutV3Recipient } from "@/types/embed";
+import type { BlockchainIncludingTestnet } from "@crossmint/common-sdk-base";
 
 export interface CrossmintHostedCheckoutV3Props {
     receipient?: EmbeddedCheckoutV3Recipient;
     locale?: Locale;
     webhookPassthroughData?: any;
     lineItems: EmbeddedCheckoutV3LineItem | EmbeddedCheckoutV3LineItem[];
-    payment: EmbeddedCheckoutV3Payment;
+    payment: HostedCheckoutV3Payment;
     appearance?: CrossmintHostedCheckoutV3Appearance;
 }
+
+export type HostedCheckoutV3Payment = {
+    receiptEmail?: string;
+    fiat: HostedCheckoutV3FiatPayment;
+    crypto: HostedCheckoutV3CryptoPayment;
+    defaultMethod?: "fiat" | "crypto";
+};
+
+export type HostedCheckoutV3FiatPayment = {
+    enabled: boolean;
+    defaultCurrency?: FiatCurrency;
+};
+
+export type HostedCheckoutV3CryptoPayment = {
+    enabled: boolean;
+    defaultChain?: BlockchainIncludingTestnet;
+    defaultCurrency?: CryptoCurrency;
+};
 
 export interface CrossmintHostedCheckoutV3Appearance {
     theme?: {
