313e39c3f2d87cfcc412e7c6e6b0b8d6eb5d3448
Pablo Egido <peg@paella.dev>
2024-12-06 17:31:49 -0500

    embedded-v2: flag component as deprecated

--- packages/client/ui/react-ui/src/components/embed/index.tsx
+++ packages/client/ui/react-ui/src/components/embed/index.tsx
@@ -8,7 +8,9 @@ import { CrossmintCryptoEmbeddedCheckout } from "./crypto/CryptoEmbeddedCheckout
 import { CrossmintFiatEmbeddedCheckout } from "./fiat/FiatEmbeddedCheckout";
 
 // TODO: Rename to CrossmintEmbeddedCheckout on v2 major publish, prior announcement since its a breaking change
-export function CrossmintPaymentElement(props: CrossmintEmbeddedCheckoutProps) {
+export function CrossmintPaymentElement_DEPRECATED(props: CrossmintEmbeddedCheckoutProps) {
+    console.error("[CrossmintPaymentElement] 🚨 This SDK is now deprecated. We encourage you to migrate to our Embedded Checkout V3, which is faster, easier to integrate and provides many more functionality. Read the docs here: https://docs.crossmint.com/nft-checkout/embedded/overview")
+
     if (isFiatEmbeddedCheckoutProps(props)) {
         return <CrossmintFiatEmbeddedCheckout {...props} />;
     }
