08d264454f3b48203cd933766a8f7b7f2f30d659
Pablo Egido <peg@paella.dev>
2024-12-06 17:42:11 -0500

    lint

--- packages/client/ui/react-ui/src/components/embed/index.tsx
+++ packages/client/ui/react-ui/src/components/embed/index.tsx
@@ -9,7 +9,9 @@ import { CrossmintFiatEmbeddedCheckout } from "./fiat/FiatEmbeddedCheckout";
 
 // TODO: Rename to CrossmintEmbeddedCheckout on v2 major publish, prior announcement since its a breaking change
 export function CrossmintPaymentElement_DEPRECATED(props: CrossmintEmbeddedCheckoutProps) {
-    console.error("[CrossmintPaymentElement_DEPRECATED] 🚨 This SDK is now deprecated. We encourage you to migrate to our Embedded Checkout V3, which is faster, easier to integrate and provides many more functionality. Read the docs here: https://docs.crossmint.com/nft-checkout/embedded/overview")
+    console.error(
+        "[CrossmintPaymentElement_DEPRECATED] 🚨 This SDK is now deprecated. We encourage you to migrate to our Embedded Checkout V3, which is faster, easier to integrate and provides many more functionality. Read the docs here: https://docs.crossmint.com/nft-checkout/embedded/overview"
+    );
 
     if (isFiatEmbeddedCheckoutProps(props)) {
         return <CrossmintFiatEmbeddedCheckout {...props} />;
