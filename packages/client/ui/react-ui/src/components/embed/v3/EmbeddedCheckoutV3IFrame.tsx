49bcfc31940615d573334cac26530365b5798c37
VeniaminC <veniamin@paella.dev>
2024-12-09 14:50:20 -0500

    resolve comments

--- packages/client/ui/react-ui/src/components/embed/v3/EmbeddedCheckoutV3IFrame.tsx
+++ packages/client/ui/react-ui/src/components/embed/v3/EmbeddedCheckoutV3IFrame.tsx
@@ -9,7 +9,7 @@ import {
 
 import { createCrossmintApiClient } from "@/utils/createCrossmintApiClient";
 import { PayerConnectionHandler } from "./crypto/PayerConnectionHandler";
-import { havePropsChanged, shouldPreserveInitialChain } from "./crypto/utils/helpers";
+import type { PayerSupportedBlockchains } from "@crossmint/common-sdk-base";
 
 const CryptoWalletConnectionHandler = lazy(() =>
     // @ts-expect-error - Error because we dont use 'module' field in tsconfig, which is expected because we use tsup to compile
@@ -27,9 +27,9 @@ export function EmbeddedCheckoutV3IFrame(props: CrossmintEmbeddedCheckoutV3Props
     const memoizedProps = useRef(props);
     if (havePropsChanged(props, memoizedProps.current)) {
         const newProps = { ...props };
-        const { shouldPreserve, updatedPayer } = shouldPreserveInitialChain(props, initialChainRef.current);
-        if (shouldPreserve) {
-            newProps.payment.crypto.payer = updatedPayer;
+        const initialChainPreservation = shouldPreserveInitialChain(props, initialChainRef.current);
+        if (initialChainPreservation.shouldPreserve) {
+            newProps.payment.crypto.payer = initialChainPreservation.updatedPayer;
         }
         memoizedProps.current = newProps;
     }
@@ -101,4 +101,27 @@ export function EmbeddedCheckoutV3IFrame(props: CrossmintEmbeddedCheckoutV3Props
             <span id="crossmint-focus-target" tabIndex={-1} />
         </>
     );
+}
+
+function havePropsChanged(
+    parentProps: CrossmintEmbeddedCheckoutV3Props,
+    currentRefProps: CrossmintEmbeddedCheckoutV3Props
+): boolean {
+    return JSON.stringify(parentProps) !== JSON.stringify(currentRefProps);
+}
+
+function shouldPreserveInitialChain(
+    props: CrossmintEmbeddedCheckoutV3Props,
+    storedInitialChain: PayerSupportedBlockchains | undefined
+): { shouldPreserve: true; updatedPayer: typeof props.payment.crypto.payer } | { shouldPreserve: false } {
+    if (props.payment.crypto.payer && storedInitialChain != null) {
+        return {
+            shouldPreserve: true,
+            updatedPayer: {
+                ...props.payment.crypto.payer,
+                initialChain: storedInitialChain,
+            },
+        };
+    }
+    return { shouldPreserve: false };
 }
