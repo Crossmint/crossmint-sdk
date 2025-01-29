82a6f48a8d9a272d0668e326ca388e995abca38d
Timur Badretdinov <accounts@destiner.io>
2025-01-29 07:44:51 +0000

    feat(smart-wallet): signatures
    * `signMessage`
    * `signTypedData`

--- packages/client/wallets/smart-wallet/src/smartWalletService.ts
+++ packages/client/wallets/smart-wallet/src/smartWalletService.ts
@@ -1,7 +1,24 @@
-import { createPublicClient, http, type Address, type Hex, type EIP1193Provider, type LocalAccount } from "viem";
+import {
+    createPublicClient,
+    concat,
+    http,
+    type Address,
+    type EIP1193Provider,
+    type Hex,
+    type LocalAccount,
+    type SignableMessage,
+    type TypedData,
+    type TypedDataDefinition,
+} from "viem";
 import { WebAuthnP256 } from "ox";
 
-import type { CrossmintApiService, CreateWalletResponse, TransactionResponse, SignerType } from "./apiService";
+import type {
+    CrossmintApiService,
+    CreateWalletResponse,
+    TransactionResponse,
+    Signer,
+    SignatureReponse,
+} from "./apiService";
 import type { SmartWalletChain } from "./evm/chains";
 import type { SmartWalletClient } from "./evm/smartWalletClient";
 import { EVMSmartWallet } from "./evm/wallet";
@@ -58,12 +75,101 @@ export class SmartWalletService {
                 throw new Error("Not implemented");
             },
 
-            signMessage: async () => {
-                throw new Error("Not implemented");
+            signMessage: async (parameters: { message: SignableMessage }) => {
+                if (typeof parameters.message !== "string") {
+                    throw new Error("Message must be a string");
+                }
+                const signerLocator = await this.getSignerLocator(adminSigner);
+
+                // Create signature
+                const signatureCreationResponse = await this.crossmintApiService.createSignature(address, {
+                    type: "evm-message",
+                    params: {
+                        message: parameters.message,
+                        signer: signerLocator,
+                        chain,
+                    },
+                });
+                const signatureId = signatureCreationResponse.id;
+
+                // Approve signature
+                const pendingApprovals = signatureCreationResponse.approvals.pending;
+                if (pendingApprovals.length !== 1) {
+                    throw new Error(`Expected 1 pending approval, got ${pendingApprovals.length}`);
+                }
+                const pendingApproval = pendingApprovals[0];
+                const signature = await this.approveSignature(
+                    address,
+                    adminSigner,
+                    signatureId,
+                    pendingApproval.message
+                );
+
+                // Get signature status until success
+                let signatureResponse: SignatureReponse | null = null;
+                while (signatureResponse === null || signatureResponse.status === "pending") {
+                    signatureResponse = await this.crossmintApiService.getSignature(address, signatureId);
+                }
+
+                if (signatureResponse?.status === "failed") {
+                    throw new Error("Message signing failed");
+                }
+
+                return signature;
             },
 
-            signTypedData: async () => {
-                throw new Error("Not implemented");
+            signTypedData: async <
+                const typedData extends TypedData | Record<string, unknown>,
+                primaryType extends keyof typedData | "EIP712Domain" = keyof typedData,
+            >(parameters: TypedDataDefinition<typedData, primaryType>) => {
+                const signerLocator = await this.getSignerLocator(adminSigner);
+
+                const { domain, message, primaryType, types } = parameters;
+                if (!domain || !message || !types) {
+                    throw new Error("Invalid typed data");
+                }
+
+                // Create signature
+                const signatureCreationResponse = await this.crossmintApiService.createSignature(address, {
+                    type: "evm-typed-data",
+                    params: {
+                        typedData: {
+                          domain,
+                          message,
+                          primaryType,
+                          types: types as TypedData,
+                        },
+                        signer: signerLocator,
+                        chain,
+                        isSmartWalletSignature: false,
+                    },
+                });
+                const signatureId = signatureCreationResponse.id;
+
+                // Approve signature
+                const pendingApprovals = signatureCreationResponse.approvals.pending;
+                if (pendingApprovals.length !== 1) {
+                    throw new Error(`Expected 1 pending approval, got ${pendingApprovals.length}`);
+                }
+                const pendingApproval = pendingApprovals[0];
+                const signature = await this.approveSignature(
+                    address,
+                    adminSigner,
+                    signatureId,
+                    pendingApproval.message
+                );
+
+                // Get signature status until success
+                let signatureResponse: SignatureReponse | null = null;
+                while (signatureResponse === null || signatureResponse.status === "pending") {
+                    signatureResponse = await this.crossmintApiService.getSignature(address, signatureId);
+                }
+
+                if (signatureResponse?.status === "failed") {
+                    throw new Error("Typed data signing failed");
+                }
+
+                return signature;
             },
 
             sendTransaction: async (parameters: {
@@ -149,7 +255,7 @@ export class SmartWalletService {
         }
     }
 
-    private async getSignerLocator(signer: ExternalSigner | PasskeySigner): Promise<SignerType> {
+    private async getSignerLocator(signer: ExternalSigner | PasskeySigner): Promise<Signer> {
         if ("type" in signer && signer.type === "PASSKEY") {
             return `evm-passkey:${signer.credential.id}`;
         } else {
@@ -208,6 +314,60 @@ export class SmartWalletService {
                     },
                 ],
             });
+        }
+    }
+
+    private async approveSignature(
+        walletAddress: Address,
+        signer: ExternalSigner | PasskeySigner,
+        signatureId: string,
+        message: Hex
+    ) {
+        if ("type" in signer && signer.type === "PASSKEY") {
+            const { metadata, signature } = await WebAuthnP256.sign({
+                credentialId: signer.credential.id,
+                challenge: message,
+            });
+
+            await this.crossmintApiService.approveSignature(walletAddress, signatureId, {
+                approvals: [
+                    {
+                        metadata,
+                        signature: {
+                            r: `0x${signature.r.toString(16)}`,
+                            s: `0x${signature.s.toString(16)}`,
+                        },
+                        signer: `evm-passkey:${signer.credential.id}`,
+                    },
+                ],
+            });
+
+            // Convert signature to hex
+            return concat([`0x${signature.r.toString(16)}`, `0x${signature.s.toString(16)}`]);
+        } else {
+            const signerAddress = await this.getSignerAddress(signer);
+            const signature =
+                "type" in signer
+                    ? await signer.account.signMessage({
+                          message: {
+                              raw: message,
+                          },
+                      })
+                    : await signer.request({
+                          method: "personal_sign",
+                          params: [message, signerAddress],
+                      });
+
+            await this.crossmintApiService.approveSignature(walletAddress, signatureId, {
+                approvals: [
+                    {
+                        signature,
+                        signer: `evm-keypair:${signerAddress}`,
+                    },
+                ],
+            });
+
+            return signature;
         }
     }
 }
