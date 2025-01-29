430cfa1bedd3b6bb99d2013dbd9458cc8944c25a
Timur Badretdinov <accounts@destiner.io>
2025-01-29 05:48:25 +0000

    feat(smart-wallet): passkey signer
    * wallet creation
    * transaction sending flow

--- packages/client/wallets/smart-wallet/src/apiService.ts
+++ packages/client/wallets/smart-wallet/src/apiService.ts
@@ -7,6 +7,8 @@ import { SCW_SERVICE, API_VERSION } from "./utils/constants";
 
 export const scwLogger = new SDKLogger(SCW_SERVICE);
 
+export type SignerType = `evm-keypair:${Address}` | `evm-passkey:${string}`;
+
 interface CreateWalletParams {
     type: "evm-smart-wallet";
     config: {
@@ -28,28 +30,42 @@ interface CreateWalletParams {
     };
 }
 
-interface CreateWalletResponse {
+export interface CreateWalletResponse {
     type: "evm-smart-wallet";
     linkedUser: string;
     address: Address;
     config: {
-        adminSigner: {
-            type: "evm-keypair";
-            address: Address;
-            locator: `evm-keypair:${Address}`;
-        };
+        adminSigner:
+            | {
+                  type: "evm-keypair";
+                  locator: `evm-keypair:${Address}`;
+                  address: Address;
+              }
+            | {
+                  type: "evm-passkey";
+                  locator: `evm-passkey:${string}`;
+                  id: string;
+                  name: string;
+                  publicKey: {
+                      x: Hex;
+                      y: Hex;
+                  };
+                  validatorContractVersion: string;
+              };
     };
+}
+
+interface TransactionCall {
+    to: Address;
+    value: string;
+    data: Hex;
 }
 
 interface CreateTransactionParams {
     params: {
-        signer: `evm-keypair:${Address}`;
+        signer: SignerType;
         chain: SmartWalletChain;
-        calls: {
-            to: Address;
-            value: string;
-            data: Hex;
-        }[];
+        calls: TransactionCall[];
     };
 }
 
@@ -60,12 +76,12 @@ export interface TransactionResponse {
     approvals: {
         pending: {
             message: Hex;
-            signer: `evm-keypair:${Address}`;
+            signer: SignerType;
         }[];
         submitted: {
             message: Hex;
             signature: Hex;
-            signer: `evm-keypair:${Address}`;
+            signer: SignerType;
         }[];
     };
     onChain: {
@@ -74,11 +90,28 @@ export interface TransactionResponse {
     };
 }
 
+type Approval =
+    | {
+          signer: `evm-keypair:${Address}`;
+          signature: Hex;
+      }
+    | {
+          signer: `evm-passkey:${string}`;
+          signature: {
+            r: Hex;
+            s: Hex;
+          }
+          metadata: {
+              authenticatorData: Hex;
+              challengeIndex: number;
+              clientDataJSON: string;
+              typeIndex: number;
+              userVerificationRequired: boolean;
+          };
+      };
+
 interface ApproveTransactionParams {
-    approvals: {
-        signer: `evm-keypair:${Address}`;
-        signature: Hex;
-    }[];
+    approvals: Approval[];
 }
 
 type WalletsAPIErrorCodes = never;
