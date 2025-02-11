69418ea59916e250a8c25563820c855c011cc5e6
Alberto <146016735+alberto-crossmint@users.noreply.github.com>
2025-02-11 21:19:24 +0100

    SmartWalletsSDK: add Story testnet (#930)
    * SmartWalletsSDK: add Story testnet
    
    * double quotes
    
    * linthappy
    
    * minor

---
+++ packages/client/wallets/smart-wallet/src/blockchain/chains.ts
@@ -0,0 +1,80 @@
+import {
+    type Chain,
+    arbitrum,
+    arbitrumSepolia,
+    base,
+    baseSepolia,
+    optimism,
+    optimismSepolia,
+    polygon,
+    polygonAmoy,
+} from "viem/chains";
+
+import { BlockchainIncludingTestnet as Blockchain, type ObjectValues, objectValues } from "@crossmint/common-sdk-base";
+import { defineChain } from "viem";
+
+const storyOdyssey = defineChain({
+    id: 1516,
+    name: "Story Odyssey",
+    nativeCurrency: {
+        decimals: 18,
+        name: "IP",
+        symbol: "IP",
+    },
+    rpcUrls: {
+        default: { http: ["https://rpc.odyssey.storyrpc.io"] },
+    },
+    blockExplorers: {
+        default: {
+            name: "Story Odyssey Explorer",
+            url: "https://odyssey.storyscan.xyz",
+        },
+    },
+    testnet: true,
+});
+
+export const SmartWalletTestnet = {
+    BASE_SEPOLIA: Blockchain.BASE_SEPOLIA,
+    POLYGON_AMOY: Blockchain.POLYGON_AMOY,
+    OPTIMISM_SEPOLIA: Blockchain.OPTIMISM_SEPOLIA,
+    ARBITRUM_SEPOLIA: Blockchain.ARBITRUM_SEPOLIA,
+    STORY_TESTNET: Blockchain.STORY_TESTNET,
+} as const;
+export type SmartWalletTestnet = ObjectValues<typeof SmartWalletTestnet>;
+export const SMART_WALLET_TESTNETS = objectValues(SmartWalletTestnet);
+
+export const SmartWalletMainnet = {
+    BASE: Blockchain.BASE,
+    POLYGON: Blockchain.POLYGON,
+    OPTIMISM: Blockchain.OPTIMISM,
+    ARBITRUM: Blockchain.ARBITRUM,
+} as const;
+export type SmartWalletMainnet = ObjectValues<typeof SmartWalletMainnet>;
+export const SMART_WALLET_MAINNETS = objectValues(SmartWalletMainnet);
+
+export const SmartWalletChain = {
+    ...SmartWalletTestnet,
+    ...SmartWalletMainnet,
+} as const;
+export type SmartWalletChain = ObjectValues<typeof SmartWalletChain>;
+export const SMART_WALLET_CHAINS = objectValues(SmartWalletChain);
+
+export function isTestnetChain(chain: SmartWalletChain): chain is SmartWalletTestnet {
+    return (SMART_WALLET_TESTNETS as any).includes(chain);
+}
+
+export function isMainnetChain(chain: SmartWalletChain): chain is SmartWalletMainnet {
+    return (SMART_WALLET_MAINNETS as any).includes(chain);
+}
+
+export const viemNetworks: Record<SmartWalletChain, Chain> = {
+    polygon: polygon,
+    "polygon-amoy": polygonAmoy,
+    base: base,
+    "base-sepolia": baseSepolia,
+    optimism: optimism,
+    "optimism-sepolia": optimismSepolia,
+    arbitrum: arbitrum,
+    "arbitrum-sepolia": arbitrumSepolia,
+    "story-testnet": storyOdyssey,
+};
