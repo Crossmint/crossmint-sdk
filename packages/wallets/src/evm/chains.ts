4e3de34ebb5af407e73944d9ea70de2454c49c86
Timur Badretdinov <accounts@destiner.io>
2025-02-19 16:31:19 +0000

    fix: rewrite EVM chains

--- packages/wallets/src/evm/chains.ts
+++ packages/wallets/src/evm/chains.ts
@@ -1,34 +1,31 @@
-import { BlockchainIncludingTestnet as Blockchain, objectValues, type ObjectValues } from "@crossmint/common-sdk-base";
+import { BlockchainIncludingTestnet as Blockchain } from "@crossmint/common-sdk-base";
 
-const SmartWalletTestnet = {
-    BASE_SEPOLIA: Blockchain.BASE_SEPOLIA,
-    POLYGON_AMOY: Blockchain.POLYGON_AMOY,
-    OPTIMISM_SEPOLIA: Blockchain.OPTIMISM_SEPOLIA,
-    ARBITRUM_SEPOLIA: Blockchain.ARBITRUM_SEPOLIA,
-} as const;
-type SmartWalletTestnet = ObjectValues<typeof SmartWalletTestnet>;
-const SMART_WALLET_TESTNETS: readonly SmartWalletChain[] = objectValues(SmartWalletTestnet);
+const SMART_WALLET_TESTNET_CHAINS = [
+    Blockchain.BASE_SEPOLIA,
+    Blockchain.POLYGON_AMOY,
+    Blockchain.OPTIMISM_SEPOLIA,
+    Blockchain.ARBITRUM_SEPOLIA,
+];
 
-const SmartWalletMainnet = {
-    BASE: Blockchain.BASE,
-    POLYGON: Blockchain.POLYGON,
-    OPTIMISM: Blockchain.OPTIMISM,
-    ARBITRUM: Blockchain.ARBITRUM,
-} as const;
-type SmartWalletMainnet = ObjectValues<typeof SmartWalletMainnet>;
-const SMART_WALLET_MAINNETS: readonly SmartWalletChain[] = objectValues(SmartWalletMainnet);
+const SMART_WALLET_MAINNET_CHAINS = [
+    Blockchain.BASE,
+    Blockchain.POLYGON,
+    Blockchain.OPTIMISM,
+    Blockchain.ARBITRUM,
+];
 
-const SmartWalletChain = {
-    ...SmartWalletTestnet,
-    ...SmartWalletMainnet,
-} as const;
+type SmartWalletTestnet = (typeof SMART_WALLET_TESTNET_CHAINS)[number];
+type SmartWalletMainnet = (typeof SMART_WALLET_MAINNET_CHAINS)[number];
+export type SmartWalletChain = SmartWalletTestnet | SmartWalletMainnet;
 
-export function isTestnetChain(chain: SmartWalletChain): chain is SmartWalletTestnet {
-    return SMART_WALLET_TESTNETS.includes(chain);
+export function isTestnetChain(
+    chain: SmartWalletChain
+): chain is SmartWalletTestnet {
+    return (SMART_WALLET_TESTNET_CHAINS as SmartWalletChain[]).includes(chain);
 }
 
-export function isMainnetChain(chain: SmartWalletChain): chain is SmartWalletMainnet {
-    return SMART_WALLET_MAINNETS.includes(chain);
+export function isMainnetChain(
+    chain: SmartWalletChain
+): chain is SmartWalletMainnet {
+    return (SMART_WALLET_MAINNET_CHAINS as SmartWalletChain[]).includes(chain);
 }
-
-export type SmartWalletChain = ObjectValues<typeof SmartWalletChain>;
