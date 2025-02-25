56991cfd8f3a600e0feee68ab5c056363ed6400b
Timur Badretdinov <accounts@destiner.io>
2025-02-25 12:56:34 +0000

    fix: set up API code generation

---
+++ packages/wallets/openapi-ts.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+    input: "src/openapi.json",
+    output: "src/api/gen",
+    plugins: [
+        "@hey-api/client-fetch",
+        "zod",
+        {
+            name: "@hey-api/sdk",
+            validator: false,
+        },
+    ],
+});
