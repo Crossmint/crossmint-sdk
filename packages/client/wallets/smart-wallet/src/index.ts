914ed4d8a770b044f98dc06477ebdde36faabd23
Timur Badretdinov <accounts@destiner.io>
2025-02-25 07:31:15 +0000

    fix: format

--- packages/client/wallets/smart-wallet/src/index.ts
+++ packages/client/wallets/smart-wallet/src/index.ts
@@ -1,9 +1,23 @@
-export { blockchainToChainId, EVMBlockchainIncludingTestnet as Chain } from "@crossmint/common-sdk-base";
+export {
+    blockchainToChainId,
+    EVMBlockchainIncludingTestnet as Chain,
+} from "@crossmint/common-sdk-base";
 
 // Types
-export type { ViemAccount, PasskeySigner, ExternalSigner, WalletParams, UserParams } from "./smartWalletService";
+export type {
+    ViemAccount,
+    PasskeySigner,
+    ExternalSigner,
+    WalletParams,
+    UserParams,
+} from "./smartWalletService";
 export type { SmartWalletChain as EVMSmartWalletChain } from "./evm/chains";
-export type { TransferType, ERC20TransferType, NFTTransferType, SFTTransferType } from "./types/transfer";
+export type {
+    TransferType,
+    ERC20TransferType,
+    NFTTransferType,
+    SFTTransferType,
+} from "./types/transfer";
 export type { SmartWalletSDKInitParams } from "./sdk";
 
 // Errors
