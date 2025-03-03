bdf54499abaed010439219a06a3302a162b976e2
Timur Badretdinov <accounts@destiner.io>
2025-03-03 11:37:31 +0000

    fix: remove API key

--- packages/wallets/src/sdk.test.ts
+++ packages/wallets/src/sdk.test.ts
@@ -18,7 +18,7 @@ describe("WalletSDK", () => {
         // Reset mocks before each test
         vi.clearAllMocks();
         sdk = WalletSDK.from({
-            apiKey: "sk_staging_A4vDwAp4t5az6fVQMpQK6qapBnAqgpxrrD35TaFQnyKgxehNbd959uZeaHjNCadWDXrgLRAK1CxeasZjtYEq4TbFkKMBBvbQ9oinAxQf8LbHsSYW2DMzT8fBko3YGLq9t7ZiXZjmgkTioxGVUUjyLtWLeBKwNUDLgpshWjaoR7pKRnSE9SqhwjQbiK62VKiBTdA3KvHsyG9k8mLMcKrDyfXp",
+            apiKey: "sk_staging_test",
         });
     });
 
