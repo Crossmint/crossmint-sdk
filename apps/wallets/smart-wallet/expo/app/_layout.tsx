491bdef83b0a5365bb576edb232530e21754d42e
Timur Badretdinov <accounts@destiner.io>
2025-03-30 14:46:52 +0100

    feat: init Expo wallets demo

---
+++ apps/wallets/smart-wallet/expo/app/_layout.tsx
@@ -0,0 +1,19 @@
+import { CrossmintProvider, CrossmintWalletProvider } from "@crossmint/client-sdk-react-native-ui";
+import { Stack } from "expo-router";
+import type { ReactNode } from "react";
+
+export default function RootLayout() {
+    return (
+        <CrossmintProviders>
+            <Stack />
+        </CrossmintProviders>
+    );
+}
+
+function CrossmintProviders({ children }: { children: ReactNode }) {
+    return (
+        <CrossmintProvider apiKey={process.env.EXPO_PUBLIC_CROSSMINT_API_KEY ?? ""}>
+            <CrossmintWalletProvider>{children}</CrossmintWalletProvider>
+        </CrossmintProvider>
+    );
+}
---
+++ apps/wallets/smart-wallet/expo/metro.config.js
@@ -0,0 +1,19 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+    if (moduleName === "crypto") {
+        // when importing crypto, resolve to react-native-quick-crypto
+        return context.resolveRequest(
+            context,
+            "react-native-quick-crypto",
+            platform
+        );
+    }
+    // otherwise chain to the standard Metro resolver.
+    return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
