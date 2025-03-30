1bbd6efae9c2ba0aa21615e1c25a753f011eae83
Timur Badretdinov <accounts@destiner.io>
2025-03-30 15:01:13 +0100

    fix: format

--- apps/wallets/smart-wallet/expo/metro.config.js
+++ apps/wallets/smart-wallet/expo/metro.config.js
@@ -6,11 +6,7 @@ const config = getDefaultConfig(__dirname);
 config.resolver.resolveRequest = (context, moduleName, platform) => {
     if (moduleName === "crypto") {
         // when importing crypto, resolve to react-native-quick-crypto
-        return context.resolveRequest(
-            context,
-            "react-native-quick-crypto",
-            platform
-        );
+        return context.resolveRequest(context, "react-native-quick-crypto", platform);
     }
     // otherwise chain to the standard Metro resolver.
     return context.resolveRequest(context, moduleName, platform);
