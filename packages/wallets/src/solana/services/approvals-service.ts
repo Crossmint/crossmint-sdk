d38a505bbd26470c34c27d2e154f9916cbfc2121
jmderby <jonathan.temp@paella.dev>
2025-04-02 15:54:02 -0700

    Sol Transactions: Fix admin signer for keypair

--- packages/wallets/src/solana/services/approvals-service.ts
+++ packages/wallets/src/solana/services/approvals-service.ts
@@ -1,4 +1,5 @@
 import bs58 from "bs58";
+import { VersionedMessage, VersionedTransaction } from "@solana/web3.js";
 import type { ApiClient, CreateTransactionSuccessResponse, SolanaWalletLocator } from "@/api";
 import type { SolanaNonCustodialSigner } from "../types/signers";
 import { PendingApprovalsError, InvalidSignerError, TransactionFailedError } from "../../utils/errors";
@@ -24,8 +25,16 @@ export class SolanaApprovalsService {
                         `Signer ${approval.signer} is required for the transaction but was not found in the signer list`
                     );
                 }
+                // Convert the decoded transaction message from server back into a VersionedTransaction
+                const messageBytes = bs58.decode(approval.message);
+                const message = VersionedMessage.deserialize(messageBytes);
+                const transaction = new VersionedTransaction(message);
+                // Sign the transaction (we can't use signMessage on transactions, so we need to sign the transaction directly)
+                const signedTxn = await signer.signTransaction(transaction);
+                // Get the signature from the signed transaction
+                const signature = this.retrieveValidSignature(signedTxn);
                 return {
-                    signature: bs58.encode(await signer.signMessage(bs58.decode(approval.message))),
+                    signature,
                     signer: approval.signer,
                 };
             })
@@ -40,5 +49,22 @@ export class SolanaApprovalsService {
             throw new PendingApprovalsError("Still has pending approvals, please submit all approvals");
         }
         return transaction;
+    }
+
+    private retrieveValidSignature(signedTxn: VersionedTransaction) {
+        let signatureObj = null;
+        for (let i = 0; i < signedTxn.signatures.length; i++) {
+            const sig = signedTxn.signatures[i];
+            // Check if this signature is non-zero
+            if (Object.values(sig).some((byte) => byte !== 0)) {
+                signatureObj = sig;
+                break;
+            }
+        }
+        if (!signatureObj) {
+            throw new Error("No valid signature found in the transaction");
+        }
+        const signatureBytes = new Uint8Array(Object.values(signatureObj));
+        return bs58.encode(signatureBytes);
     }
 }
