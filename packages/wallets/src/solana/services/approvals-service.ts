4fabb2e9ad7b593c6847699960ff1465dc79dc1c
Alberto García Planes <alberto@paella.dev>
2025-04-14 15:42:31 +0200

    SSW: fix signature error in some browser wallets

--- packages/wallets/src/solana/services/approvals-service.ts
+++ packages/wallets/src/solana/services/approvals-service.ts
@@ -1,10 +1,26 @@
 import bs58 from "bs58";
-import { PublicKey, VersionedMessage, VersionedTransaction } from "@solana/web3.js";
-import type { ApiClient, CreateTransactionSuccessResponse, SolanaWalletLocator } from "@/api";
+import {
+    PublicKey,
+    Transaction,
+    VersionedMessage,
+    VersionedTransaction,
+} from "@solana/web3.js";
+import type {
+    ApiClient,
+    CreateTransactionSuccessResponse,
+    SolanaWalletLocator,
+} from "@/api";
 import type { SolanaNonCustodialSigner } from "../types/signers";
-import { PendingApprovalsError, InvalidSignerError, TransactionFailedError } from "../../utils/errors";
+import {
+    PendingApprovalsError,
+    InvalidSignerError,
+    TransactionFailedError,
+} from "../../utils/errors";
+import type { WalletsV1Alpha2TransactionResponseDto } from "@/api/gen";
 
-type PendingApproval = NonNullable<NonNullable<CreateTransactionSuccessResponse["approvals"]>["pending"]>[number];
+type PendingApproval = NonNullable<
+    NonNullable<CreateTransactionSuccessResponse["approvals"]>["pending"]
+>[number];
 
 export class SolanaApprovalsService {
     constructor(
@@ -13,53 +29,88 @@ export class SolanaApprovalsService {
     ) {}
 
     public async approve(
-        transactionId: string,
+        transaction: WalletsV1Alpha2TransactionResponseDto,
         pendingApprovals: Array<PendingApproval>,
         signers: Array<SolanaNonCustodialSigner>
     ) {
+        if (
+            transaction.walletType !== "solana-smart-wallet" &&
+            transaction.walletType !== "solana-mpc-wallet"
+        ) {
+            throw new Error(
+                `Unsupported wallet type: ${transaction.walletType}`
+            );
+        }
         const approvals = await Promise.all(
             pendingApprovals.map(async (approval) => {
-                const signer = signers.find((s) => approval.signer.includes(s.address));
+                const signer = signers.find((s) =>
+                    approval.signer.includes(s.address)
+                );
                 if (signer == null) {
                     throw new InvalidSignerError(
                         `Signer ${approval.signer} is required for the transaction but was not found in the signer list`
                     );
                 }
-                // Convert the decoded transaction message from server back into a VersionedTransaction
-                const messageBytes = bs58.decode(approval.message);
-                const message = VersionedMessage.deserialize(messageBytes);
-                const transaction = new VersionedTransaction(message);
+                const transactionBytes = bs58.decode(
+                    transaction.onChain.transaction
+                );
+                const deserializedTransaction =
+                    VersionedTransaction.deserialize(transactionBytes);
                 // Sign the transaction (we can't use signMessage on transactions, so we need to sign the transaction directly)
-                const signedTxn = await signer.signTransaction(transaction);
+                const signedTxn = await signer.signTransaction(
+                    deserializedTransaction
+                );
                 // Get the signature from the signed transaction
                 const walletPublicKey = new PublicKey(signer.address);
-                const signature = this.retrieveValidSignature(signedTxn, walletPublicKey);
+
+                const signature = this.retrieveValidSignature(
+                    signedTxn,
+                    walletPublicKey
+                );
+                console.log("Signature from tx", signature);
                 return {
                     signature,
                     signer: approval.signer,
                 };
             })
         );
-        const transaction = await this.apiClient.approveTransaction(this.walletLocator, transactionId, {
-            approvals,
-        });
-        if (transaction.error) {
-            throw new TransactionFailedError(JSON.stringify(transaction));
+        const approvedTransaction = await this.apiClient.approveTransaction(
+            this.walletLocator,
+            transaction.id,
+            {
+                approvals,
+            }
+        );
+        if (approvedTransaction.error) {
+            throw new TransactionFailedError(
+                JSON.stringify(approvedTransaction)
+            );
         }
-        if (transaction.status === "awaiting-approval") {
-            throw new PendingApprovalsError("Still has pending approvals, please submit all approvals");
+        if (approvedTransaction.status === "awaiting-approval") {
+            throw new PendingApprovalsError(
+                "Still has pending approvals, please submit all approvals"
+            );
         }
-        return transaction;
+        return approvedTransaction;
     }
 
-    private retrieveValidSignature(signedTxn: VersionedTransaction, signerPublicKey: PublicKey) {
-        const signerIndex = signedTxn.message.staticAccountKeys.findIndex((key) => key.equals(signerPublicKey));
+    private retrieveValidSignature(
+        signedTxn: VersionedTransaction,
+        signerPublicKey: PublicKey
+    ) {
+        const signerIndex = signedTxn.message.staticAccountKeys.findIndex(
+            (key) => key.equals(signerPublicKey)
+        );
         if (signerIndex === -1) {
-            throw new TransactionFailedError("Wallet public key not found in transaction signers");
+            throw new TransactionFailedError(
+                "Wallet public key not found in transaction signers"
+            );
         }
         const signature = signedTxn.signatures[signerIndex];
         if (signature == null) {
-            throw new TransactionFailedError("No valid signature found in the transaction");
+            throw new TransactionFailedError(
+                "No valid signature found in the transaction"
+            );
         }
         const signatureBytes = new Uint8Array(Object.values(signature));
         return bs58.encode(signatureBytes);
