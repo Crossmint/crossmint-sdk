3e504900b76ed6d1f6d3d153fdf62aaa6912d03f
Alberto Elias <hi@albertoelias.me>
2025-04-28 21:45:55 -0400

    fixes react-ui build

--- packages/client/ui/react-ui/src/types/wallet.ts
+++ packages/client/ui/react-ui/src/types/wallet.ts
@@ -1,10 +1,4 @@
-import type { CustomEVMAdminSigner, CustomSolanaAdminSigner } from "@/hooks/useDynamicConnect";
 import type { EVMSignerInput, SolanaSignerInput } from "@crossmint/wallets-sdk";
 
 export type { GetOrCreateWalletProps, CrossmintAuthEmbeddedWallets } from "@crossmint/client-sdk-react-base";
-export type GetOrCreateWalletAdminSigner =
-    | CustomEVMAdminSigner
-    | CustomSolanaAdminSigner
-    | EVMSignerInput
-    | SolanaSignerInput
-    | undefined;
+export type GetOrCreateWalletAdminSigner = EVMSignerInput | SolanaSignerInput | undefined;
