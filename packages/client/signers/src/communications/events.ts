524126f63cf055025585a66ea636f82c449ee7bf
Alberto García Planes <alberto@paella.dev>
2025-04-30 15:18:57 +0200

    delete old events

--- packages/client/signers/src/communications/events.ts
+++ packages/client/signers/src/communications/events.ts
@@ -5,27 +5,15 @@ import {
     GetAttestationPayloadSchema,
     GetPublicKeyPayloadSchema,
     SendEncryptedOtpPayloadSchema,
-    SignMessagePayloadSchema,
     SignPayloadSchema,
-    SignTransactionPayloadSchema,
 } from "./schemas";
 
-export const SIGNER_EVENTS = [
-    "create-signer",
-    "get-attestation",
-    "sign-message", // Deprecated, use sign instead
-    "sign-transaction", // Deprecated, use sign instead
-    "send-otp",
-    "get-public-key",
-    "sign",
-] as const;
+export const SIGNER_EVENTS = ["create-signer", "get-attestation", "send-otp", "get-public-key", "sign"] as const;
 export type SignerIFrameEventName = (typeof SIGNER_EVENTS)[number];
 
 export const signerInboundEvents = {
     "request:create-signer": CreateSignerPayloadSchema.request,
     "request:get-attestation": GetAttestationPayloadSchema.request,
-    "request:sign-message": SignMessagePayloadSchema.request,
-    "request:sign-transaction": SignTransactionPayloadSchema.request,
     "request:send-otp": SendEncryptedOtpPayloadSchema.request,
     "request:get-public-key": GetPublicKeyPayloadSchema.request,
     "request:sign": SignPayloadSchema.request,
@@ -34,8 +22,6 @@ export const signerInboundEvents = {
 export const signerOutboundEvents = {
     "response:create-signer": CreateSignerPayloadSchema.response,
     "response:get-attestation": GetAttestationPayloadSchema.response,
-    "response:sign-message": SignMessagePayloadSchema.response,
-    "response:sign-transaction": SignTransactionPayloadSchema.response,
     "response:send-otp": SendEncryptedOtpPayloadSchema.response,
     "response:get-public-key": GetPublicKeyPayloadSchema.response,
     "response:sign": SignPayloadSchema.response,
