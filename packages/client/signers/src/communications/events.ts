3c371bee912c5067dd2a6d348dfb555e9e220d15
Devlyn Dorfer <devlyndorfer@gmail.com>
2025-05-16 14:59:22 -0400

    add get status event

--- packages/client/signers/src/communications/events.ts
+++ packages/client/signers/src/communications/events.ts
@@ -4,11 +4,12 @@ import {
     CreateSignerPayloadSchema,
     GetAttestationPayloadSchema,
     GetPublicKeyPayloadSchema,
+    GetStatusPayloadSchema,
     SendEncryptedOtpPayloadSchema,
     SignPayloadSchema,
 } from "./schemas";
 
-export const SIGNER_EVENTS = ["create-signer", "get-attestation", "send-otp", "get-public-key", "sign"] as const;
+export const SIGNER_EVENTS = ["create-signer", "get-attestation", "send-otp", "get-public-key", "sign", "get-status"] as const;
 export type SignerIFrameEventName = (typeof SIGNER_EVENTS)[number];
 
 export const signerInboundEvents = {
@@ -17,6 +18,7 @@ export const signerInboundEvents = {
     "request:send-otp": SendEncryptedOtpPayloadSchema.request,
     "request:get-public-key": GetPublicKeyPayloadSchema.request,
     "request:sign": SignPayloadSchema.request,
+    "request:get-status": GetStatusPayloadSchema.request,
 } as const;
 
 export const signerOutboundEvents = {
@@ -25,6 +27,7 @@ export const signerOutboundEvents = {
     "response:send-otp": SendEncryptedOtpPayloadSchema.response,
     "response:get-public-key": GetPublicKeyPayloadSchema.response,
     "response:sign": SignPayloadSchema.response,
+		"response:get-status": GetStatusPayloadSchema.response,
 } as const;
 
 export type SignerInputEvent<E extends SignerIFrameEventName> = z.infer<(typeof signerInboundEvents)[`request:${E}`]>;
