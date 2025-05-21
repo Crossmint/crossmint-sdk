ad0b65d5d05d1e9c955eb31ebaad44a2d2ec08fe
Alberto García Planes <alberto@paella.dev>
2025-05-21 17:40:46 +0200

    fix

--- packages/client/ui/react-ui/src/providers/signers/CrossmintSignerProvider.tsx
+++ packages/client/ui/react-ui/src/providers/signers/CrossmintSignerProvider.tsx
@@ -143,9 +143,6 @@ export function CrossmintSignerProvider({
                             jwt,
                             apiKey,
                         },
-                        data: {
-                            keyType: "ed25519",
-                        },
                     },
                 });
 
