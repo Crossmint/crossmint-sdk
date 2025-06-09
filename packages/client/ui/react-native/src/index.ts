a3c9d8e8126255da1423133ddde925879eda3890
Jonathan <jonathan.temp@paella.dev>
2025-06-09 11:12:33 -0700

    Wallets 2.5 react native follow up fixes (#1179)
    * React Native: fix expo demo and native provider
    
    * added changeset
    
    * revert polyfills
    
    * revert package

--- packages/client/ui/react-native/src/index.ts
+++ packages/client/ui/react-native/src/index.ts
@@ -4,3 +4,5 @@ export * from "./providers";
 export type { CrossmintEvent, CrossmintEventMap } from "@crossmint/client-sdk-base";
 
 export type { SDKExternalUser, OAuthProvider } from "@crossmint/common-sdk-auth";
+
+export type { Balances, DelegatedSigner } from "@crossmint/client-sdk-react-base";
--- packages/client/ui/react-native/src/providers/CrossmintProvider.tsx
+++ packages/client/ui/react-native/src/providers/CrossmintProvider.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode } from "react";
 import Constants from "expo-constants";
 import { CrossmintProvider as BaseCrossmintProvider } from "@crossmint/client-sdk-react-base";
-import type { Crossmint } from "@crossmint/common-sdk-base";
+import type { CrossmintConfig } from "@crossmint/common-sdk-base";
 
 export function CrossmintProvider({
     children,
     apiKey,
     overrideBaseUrl,
-}: Omit<Crossmint, "jwt" | "appId"> & {
+}: Pick<CrossmintConfig, "apiKey" | "overrideBaseUrl"> & {
     children: ReactNode;
 }) {
     // Get app ID from Expo constants
