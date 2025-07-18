2ad2a06e8bae0e1c3b96e814680155a75ab54be2
maxisch <maximiliano.schultheis@gmail.com>
2025-07-18 10:12:21 -0300

    WAL-5344: Harden transport for signers flow (#1282)
    * harden transport for signers flow
    
    * changeset
    
    * extract to type

--- packages/client/window/src/index.ts
+++ packages/client/window/src/index.ts
@@ -3,3 +3,4 @@ export * from "./handshake";
 export type { Transport, SimpleMessageEvent } from "./transport/Transport";
 export type { EventMap } from "./EventEmitter";
 export { generateRandomString } from "./utils/generateRandomString";
+export { SignersWindowTransport } from "./transport/SignersWindowTransport";
