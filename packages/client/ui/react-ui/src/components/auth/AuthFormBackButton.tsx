d5be2429f65eed7fa12f3fed8bd66a9dbc530d4b
jmderby <jonathan.temp@paella.dev>
2025-07-17 20:58:04 -0700

    oops

--- packages/client/ui/react-ui/src/components/auth/AuthFormBackButton.tsx
+++ packages/client/ui/react-ui/src/components/auth/AuthFormBackButton.tsx
@@ -25,7 +25,6 @@ export const AuthFormBackButton = ({
             }}
             {...props}
         >
-            penis
             <LeftArrowIcon className={tw("w-6 h-6")} style={{ color: iconColor }} />
         </button>
     );
