1063cb1f17515705bfdd9551ccae681ffc58f489
jmderby <jonathan.temp@paella.dev>
2025-07-23 23:48:43 -0700

    React SDK: remove twind once and for all

---
+++ packages/client/ui/react-ui/src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    "cm-text-primary": "#00150D",
+    "cm-text-secondary": "#67797F",
+    "cm-background-primary": "#FFFFFF",
+    "cm-muted-primary": "#F0F2F4",
+    "cm-hover": "#E9ECF0",
+    "cm-border": "#D9D9D9",
+    "cm-link": "#1A74E9",
+    "cm-accent": "#04AA6D",
+} as const;
