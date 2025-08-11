0ab4711b1fb67f888e8757f464ab274e29fbee7c
Maxwell Fortney <maxwell@paella.dev>
2025-08-11 18:01:22 -0400

    clean

--- packages/client/ui/react-native/app.plugin.js
+++ packages/client/ui/react-native/app.plugin.js
@@ -1,1 +1,1 @@
-module.exports = require("./dist/plugin/withGooglePay.cjs");
+module.exports = require("./dist/plugin/withGooglePay");
--- packages/client/ui/react-native/src/plugin/withGooglePay.ts
+++ packages/client/ui/react-native/src/plugin/withGooglePay.ts
@@ -1,4 +1,7 @@
-import { type ConfigPlugin, withAndroidManifest, createRunOncePlugin } from "@expo/config-plugins";
+import { type ConfigPlugin, withAndroidManifest, createRunOncePlugin, AndroidConfig } from "@expo/config-plugins";
+import { addQueryToAndroidManifest } from "./mods/addQueryToAndroidManifest";
+
+const { addMetaDataItemToMainApplication, getMainApplicationOrThrow } = AndroidConfig.Manifest;
 
 const pkg = require("@crossmint/client-sdk-react-native-ui/package.json");
 
@@ -6,97 +9,52 @@ export interface GooglePayPluginProps {
     enableGooglePay?: boolean;
 }
 
-const withGooglePayInternal: ConfigPlugin<GooglePayPluginProps> = (config, options: GooglePayPluginProps = {}) => {
+const withGooglePay: ConfigPlugin<GooglePayPluginProps> = (
+    config,
+    options: GooglePayPluginProps = { enableGooglePay: false }
+) => {
     if (!options.enableGooglePay) {
         return config;
     }
 
     return withAndroidManifest(config, (config) => {
-        const androidManifest = config.modResults;
-        const mainApplication = androidManifest?.manifest?.application?.[0];
+        const mainApplication = getMainApplicationOrThrow(config.modResults);
 
-        if (!mainApplication) {
-            throw new Error("Unable to find main application in AndroidManifest.xml");
-        }
-
-        if (!mainApplication["meta-data"]) {
-            mainApplication["meta-data"] = [];
-        }
-
-        const googlePayMetaData = {
-            $: {
-                "android:name": "com.google.android.gms.wallet.api.enabled",
-                "android:value": "true",
-            },
-        };
-
-        const existingMetaData = mainApplication["meta-data"].find(
-            (meta: any) => meta.$?.["android:name"] === "com.google.android.gms.wallet.api.enabled"
-        );
-
-        if (!existingMetaData) {
-            mainApplication["meta-data"].push(googlePayMetaData);
-        }
-
-        if (!androidManifest.manifest.queries) {
-            androidManifest.manifest.queries = [];
-        }
-
-        const googlePayQueries = [
-            {
-                intent: [
-                    {
-                        action: [
-                            {
-                                $: {
-                                    "android:name": "org.chromium.intent.action.PAY",
-                                },
+        addMetaDataItemToMainApplication(mainApplication, "com.google.android.gms.wallet.api.enabled", "true");
+        config.modResults = addQueryToAndroidManifest(config.modResults, {
+            intent: [
+                {
+                    action: [
+                        {
+                            $: {
+                                "android:name": "org.chromium.intent.action.PAY",
                             },
-                        ],
-                    },
-                ],
-            },
-            {
-                intent: [
-                    {
-                        action: [
-                            {
-                                $: {
-                                    "android:name": "org.chromium.intent.action.IS_READY_TO_PAY",
-                                },
+                        },
+                    ],
+                },
+                {
+                    action: [
+                        {
+                            $: {
+                                "android:name": "org.chromium.intent.action.IS_READY_TO_PAY",
                             },
-                        ],
-                    },
-                ],
-            },
-            {
-                intent: [
-                    {
-                        action: [
-                            {
-                                $: {
-                                    "android:name": "org.chromium.intent.action.UPDATE_PAYMENT_DETAILS",
-                                },
+                        },
+                    ],
+                },
+                {
+                    action: [
+                        {
+                            $: {
+                                "android:name": "org.chromium.intent.action.UPDATE_PAYMENT_DETAILS",
                             },
-                        ],
-                    },
-                ],
-            },
-        ];
-
-        googlePayQueries.forEach((query) => {
-            const actionName = query.intent[0].action[0].$["android:name"];
-            const existingQuery = androidManifest.manifest.queries.find(
-                (q: any) => q.intent?.[0]?.action?.[0]?.$?.["android:name"] === actionName
-            );
-
-            if (!existingQuery) {
-                androidManifest.manifest.queries.push(query);
-            }
+                        },
+                    ],
+                },
+            ],
         });
 
         return config;
     });
 };
 
-export default createRunOncePlugin(withGooglePayInternal, pkg.name, pkg.version);
+export default createRunOncePlugin(withGooglePay, pkg.name, pkg.version);
