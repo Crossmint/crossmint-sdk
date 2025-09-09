c5b14997693588f31818d33174e4ae4b314e59be
Pablo Egido <peg@paella.dev>
2025-09-09 11:02:03 +0100

    chain: sunset apex boss barret (#1386)
    * chain: sunset apex boss barret
    
    * asd

--- packages/client/ui/react-ui/src/components/dynamic-xyz/DynamicContextProviderWrapper.tsx
+++ packages/client/ui/react-ui/src/components/dynamic-xyz/DynamicContextProviderWrapper.tsx
@@ -1,5 +1,5 @@
 import type { APIKeyEnvironmentPrefix } from "@crossmint/common-sdk-base";
-import { type DynamicContextProps, DynamicContextProvider, mergeNetworks } from "@dynamic-labs/sdk-react-core";
+import { type DynamicContextProps, DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import type { ReactNode } from "react";
 
 export interface DynamicContextProviderWrapperProps {
@@ -22,14 +22,6 @@ export default function DynamicContextProviderWrapper({
                         ? "3fc6c24e-6a8e-45f8-aae1-a87d7a027e12"
                         : "cd53135a-b32b-4704-bfca-324b665e9329",
                 cssOverrides: `.powered-by-dynamic { display: none !important; }`,
-                ...(apiKeyEnvironment === "production"
-                    ? {
-                          overrides: {
-                              evmNetworks: (defaultNetworks) =>
-                                  mergeNetworks(additionalEvmMainnetNetworks, defaultNetworks),
-                          },
-                      }
-                    : {}),
                 ...settings,
             }}
         >
@@ -37,23 +29,3 @@ export default function DynamicContextProviderWrapper({
         </DynamicContextProvider>
     );
 }
-
-const additionalEvmMainnetNetworks = [
-    {
-        blockExplorerUrls: ["ttps://explorer-proofofplay-boss-mainnet.t.conduit.xyz/"],
-        chainId: 70701,
-        chainName: "Boss Mainnet",
-        iconUrls: ["https:/www.crossmint.com/assets/ui/logos/apex.svg"],
-        name: "Boss",
-        nativeCurrency: {
-            decimals: 18,
-            name: "Ether",
-            symbol: "ETH",
-            iconUrl: "https://app.dynamic.xyz/assets/networks/eth.svg",
-        },
-        networkId: 70701,
-
-        rpcUrls: ["https://rpc.boss.proofofplay.com/"],
-        vanityName: "Boss",
-    },
-];
