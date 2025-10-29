1e53af3395b0c1b33e0ddb1008a0cb045b884307
mPaella <93682696+mPaella@users.noreply.github.com>
2025-10-29 18:13:51 -0400

    saved cards: appearance (#1475)
    * save
    
    * bump

--- packages/client/base/src/types/payment-method-management/CrossmintPaymentMethodManagementProps.ts
+++ packages/client/base/src/types/payment-method-management/CrossmintPaymentMethodManagementProps.ts
@@ -1,3 +1,11 @@
+import type { EmbeddedCheckoutV3Appearance } from "../embed";
+
 export interface CrossmintPaymentMethodManagementProps {
     jwt: string;
+    appearance?: PaymentMethodManagementAppearance;
 }
+
+export type PaymentMethodManagementAppearance = {
+    fonts?: EmbeddedCheckoutV3Appearance["fonts"];
+    variables?: EmbeddedCheckoutV3Appearance["variables"];
+};
