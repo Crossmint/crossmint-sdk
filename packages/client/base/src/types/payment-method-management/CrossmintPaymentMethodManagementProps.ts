70db17c481b356d2b392431631222bcb664ab928
Maxwell Fortney <maxwell@paella.dev>
2025-11-10 17:57:21 -0500

    save

--- packages/client/base/src/types/payment-method-management/CrossmintPaymentMethodManagementProps.ts
+++ packages/client/base/src/types/payment-method-management/CrossmintPaymentMethodManagementProps.ts
@@ -3,7 +3,19 @@ import type { EmbeddedCheckoutV3Appearance } from "../embed";
 export interface CrossmintPaymentMethodManagementProps {
     jwt: string;
     appearance?: PaymentMethodManagementAppearance;
+    onPaymentMethodSelected?: (
+        paymentMethod: CrossmintPaymentMethod
+    ) => void | Promise<void>;
 }
+
+export type CrossmintPaymentMethod = {
+    type: "card";
+    paymentMethodId: string;
+    card: {
+        brand: string;
+        last4: string;
+    };
+};
 
 export type PaymentMethodManagementAppearance = {
     fonts?: EmbeddedCheckoutV3Appearance["fonts"];
--- packages/client/base/src/types/payment-method-management/events/incoming.ts
+++ packages/client/base/src/types/payment-method-management/events/incoming.ts
@@ -4,5 +4,7 @@ export const paymentMethodManagementIncomingEvents = {
     "ui:height.changed": z.object({
         height: z.number(),
     }),
+    "payment-method:selected": z.any(),
 };
-export type PaymentMethodManagementIncomingEventMap = typeof paymentMethodManagementIncomingEvents;
+export type PaymentMethodManagementIncomingEventMap =
+    typeof paymentMethodManagementIncomingEvents;
