28656c3943b2eaa4998f7d1da27c40da7f137788
Guille Aszyn <guille.a@paella.dev>
2026-01-14 12:30:48 -0500

    fix tests

--- apps/wallets/quickstart-devkit/app/home.tsx
+++ apps/wallets/quickstart-devkit/app/home.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { WalletBalance } from "../components/balance";
 import { Permissions } from "../components/permissions";
 import { CrossmintAuthLoginButton } from "../components/login";
