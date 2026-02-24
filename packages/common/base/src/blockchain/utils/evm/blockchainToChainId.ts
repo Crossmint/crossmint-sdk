f5bcec1786919b7c0ba4dceb59a742d36271d817
VeniaminC <107143161+VeniaminC@users.noreply.github.com>
2026-02-24 12:46:51 -0500

    Update: tempo testnet details (#1547)
    * Update: tempo testnet details
    
    * changeset
    
    * Update pink-ways-cover.md

--- packages/common/base/src/blockchain/utils/evm/blockchainToChainId.ts
+++ packages/common/base/src/blockchain/utils/evm/blockchainToChainId.ts
@@ -48,7 +48,7 @@ export const BLOCKCHAIN_TO_CHAIN_ID: Record<EVMBlockchainIncludingTestnet, numbe
     plume: 98865,
     "plume-testnet": 161221135,
     "arc-testnet": 5042002,
-    "tempo-testnet": 42429,
+    "tempo-testnet": 42431,
 };
 
 export function blockchainToChainId(blockchain: EVMBlockchainIncludingTestnet) {
