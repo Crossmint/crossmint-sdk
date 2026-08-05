dd063704fbd45413464557de2d5131286c3d52c7
Robin <robin@paella.dev>
2026-08-05 21:28:42 +0000

    refactor(wallets): rename sendWithFreshJwt to sendAction

--- packages/wallets/src/signers/non-custodial/ncs-signer.ts
+++ packages/wallets/src/signers/non-custodial/ncs-signer.ts
@@ -135,7 +135,7 @@ export abstract class NonCustodialSigner implements SignerAdapter {
         // Determine if we need to authenticate the user via OTP or not
         walletsLogger.info("get-status: sending request");
         const startTime = Date.now();
-        const signerResponse = await this.sendWithFreshJwt((authData) =>
+        const signerResponse = await this.sendAction((authData) =>
             clientTEEConnection.sendAction({
                 event: "request:get-status",
                 responseEvent: "response:get-status",
@@ -254,7 +254,7 @@ export abstract class NonCustodialSigner implements SignerAdapter {
      * changed since the request started, retries once with the latest JWT. This handles
      * the case where `setJwt` is called while `sendAction` is awaiting a response.
      */
-    private async sendWithFreshJwt<T>(send: (authData: { apiKey: string; jwt: string }) => Promise<T>): Promise<T> {
+    private async sendAction<T>(send: (authData: { apiKey: string; jwt: string }) => Promise<T>): Promise<T> {
         const authData = this.createAuthData();
         const initialJwt = authData.jwt;
         const result = await send(authData);
@@ -291,7 +291,7 @@ export abstract class NonCustodialSigner implements SignerAdapter {
         const authId = this.getAuthId();
         walletsLogger.info("start-onboarding: sending request");
         const startTime = Date.now();
-        const response = await this.sendWithFreshJwt((authData) =>
+        const response = await this.sendAction((authData) =>
             handshakeParent.sendAction({
                 event: "request:start-onboarding",
                 responseEvent: "response:start-onboarding",
@@ -334,7 +334,7 @@ export abstract class NonCustodialSigner implements SignerAdapter {
             const handshakeParent = await this.getTEEConnection();
             walletsLogger.info("complete-onboarding: sending request");
             const startTime = Date.now();
-            response = await this.sendWithFreshJwt((authData) =>
+            response = await this.sendAction((authData) =>
                 handshakeParent.sendAction({
                     event: "request:complete-onboarding",
                     responseEvent: "response:complete-onboarding",
@@ -449,7 +449,7 @@ export abstract class NonCustodialSigner implements SignerAdapter {
             });
         }
 
-        const response = await this.sendWithFreshJwt((authData) =>
+        const response = await this.sendAction((authData) =>
             exportTEEConnection.sendAction({
                 event: "request:export-signer",
                 responseEvent: "response:export-signer",
